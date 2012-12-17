a4a1c19385bb7174857d8d85ad25627aca50151d
Lars Greiss <kufikugel@googlemail.com>
2012-12-17 05:11:21 -0600

    SystemUI: statusbar clock date customization (1/2)
    This commit add support for:
    
    - Clock enable/disable
    - Clock position
    - Clock seconds
    - AM/PM styles multilang support
    - Date format
    - Date style
    
    mydongistiny:
    -Ported to Pie
    -Updated Left/Right Clock
    -Fixed heads-up enabling left clock when disabled
    
    spezi77 edits:
    - Updated for Android10
    - Adapted for ContentObserver
    - Add back clock seconds (why not though?)
    - Add clock center/right position
    - Add date right/left position
    - Fixed "Clock is double after reboot or update"
    - Clock: Fix crash in formatted string with date on right
    - CollapsedStatusbar: Fix phantom spaces in status bar
    - Make date/clock view play nice with collapsed/expanded statusbar
    - Fixed "system icons appear twice on lockscreen"
    - Fixed "Clock appears despite that it has been set to hidden"
    - Play nice with collapsed/expanded statusbar
    - Play nice with heads-up notifications
    
    Change-Id: I73d854ed5cfb57fa19e4b5554bb10cb2e1ce9b65
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -271,6 +271,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
 
         mClockView = findViewById(R.id.clock);
         mClockView.setOnClickListener(this);
+	mClockView.setQsHeader();
         mDateView = findViewById(R.id.date);
         mSpace = findViewById(R.id.space);
 
@@ -493,6 +494,7 @@ public class QuickStatusBarHeader extends RelativeLayout implements
         if (mExpanded == expanded) return;
         mExpanded = expanded;
         mHeaderQsPanel.setExpanded(expanded);
+	mDateView.setVisibility(mClockView.isClockDateEnabled() ? View.INVISIBLE : View.VISIBLE);
         updateEverything();
     }
 
