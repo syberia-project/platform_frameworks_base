96c740846531c8f7e760ca72194cfec191c52d9d
Evisceration <eviscerationls@gmail.com>
2015-01-01 14:29:50 +0000

    QuickSettings: Add Haptic Feedback to tiles [1/2]
    Change-Id: I792a00a2689a572110963db00362f8e9d77b316e
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -219,7 +219,8 @@ public class SecureSettings {
         Settings.Secure.BLUETOOTH_LE_BROADCAST_PROGRAM_INFO,
         Settings.Secure.BLUETOOTH_LE_BROADCAST_CODE,
         Settings.Secure.BLUETOOTH_LE_BROADCAST_APP_SOURCE_NAME,
-        Settings.Secure.ADVANCED_REBOOT
+        Settings.Secure.ADVANCED_REBOOT,
         Settings.Secure.SHOW_BACK_ARROW_GESTURE,
+        Settings.Secure.QUICK_SETTINGS_TILES_VIBRATE,
     };
 }
