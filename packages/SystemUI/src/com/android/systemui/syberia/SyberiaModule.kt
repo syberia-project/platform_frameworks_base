6b172bcb70560b1fec546eadc547967b87eca9b5
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 02:15:28 +0300

    SystemUI: Add heads up tile
    xyyx: adapted for P and replaced drawbles
    
    Change-Id: Ied99d31630cd85bbb85f176987fbcf38d197d0cf
    Signed-off-by: xyyx <xyyx@mail.ru>
    
    [Stallix] Adapt for A13 QPR2 after b8d6c0a94834079f5598dc920a818fd4d67bf636.
    [Stallix] Adapt for A13 QPR3
    
    Change-Id: I45b26187cfff754b8eafc3b129d76c8bdfdf4ada

--- packages/SystemUI/src/com/android/systemui/syberia/SyberiaModule.kt
+++ packages/SystemUI/src/com/android/systemui/syberia/SyberiaModule.kt
@@ -21,6 +21,7 @@ import com.android.systemui.qs.tiles.LiveDisplayTile
 import com.android.systemui.qs.tiles.ReadingModeTile
 import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.CaffeineTile
+import com.android.systemui.qs.tiles.HeadsUpTile
 
 import dagger.Binds
 import dagger.Module
@@ -29,6 +30,12 @@ import dagger.multibindings.StringKey
 
 @Module
 interface SyberiaModule {
+    /** Inject HeadsUpTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(HeadsUpTile.TILE_SPEC)
+    fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
+
     /** Inject CaffeineTile into tileMap in QSModule */
     @Binds
     @IntoMap
