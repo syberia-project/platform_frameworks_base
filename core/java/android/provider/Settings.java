79990e478d470c44690ff439a2925e1081908ae9
Dalingrin <dalingrin@gmail.com>
2016-05-06 03:43:03 +0200

    SystemUI: Add double tap to sleep gesture
    Change-Id: I708338e6ac069856c3f4da1ae2fd8029a7899e1c
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4308,6 +4308,12 @@ public final class Settings {
         public static final String VOLUME_BUTTON_MUSIC_CONTROL = "volume_button_music_control";
 
         /**
+         * Enable statusbar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
