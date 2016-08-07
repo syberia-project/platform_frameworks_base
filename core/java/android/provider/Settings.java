205bde824c47bc41afce3fb894334ced6f31d982
Randall Rushing <randall.rushing@gmail.com>
2016-08-07 12:06:07 -0700

    Hardware key disable support
    Change-Id: Ib876f6ef50e9ba42e9aa862f50e871b3f1e39ae0
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5684,6 +5684,12 @@ public final class Settings {
         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
+         * Disable hw buttons - actions, brightness, haptic feedback, overflow menu
+         * @hide
+         */
+        public static final String HARDWARE_KEYS_DISABLE = "hardware_keys_disable";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
