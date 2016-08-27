b8889f6891b204e7f2929b608f1571b049a46fa8
Nicholas Chum <nicholaschum@gmail.com>
2016-08-27 10:56:46 -0400

    Themes: Expose notification icon colors
    * Forward-port and adapt to Android Pie
    * Only exposing app icon+overflow text color because app name color isn't dynamic in Android Pie
    * Skipped sender text exposure as it's not hardcoded to BLACK in Android Pie
    
    * Also changed booleans and color resources according to current icon-only schema
    * This removes notification_text_default_color and replaces old config_allowNotificationIconTextTinting
      with config_allowNotificationIconTinting because app name isn't dynamic anymore
    
    * This commit is squashed forward-port of following commits:
    
      SubstratumResources/platform_frameworks_base@f18f319
      Author: Nicholas Chum <nicholaschum@gmail.com>
      Date: Sat, 27 Aug 2016 10:56:46 -0400
      Commit: Themes: Notifications: Expose a bool to disable dynamic colors
    
      * This commit allows a themer to overlay a boolean value in config.xml to
        disable dynamic colors applied to the app title and app icon of each
        notification.
    
        PS6:
        Separate app title and small icon colors for more flexibility.
        Expose hardcoded sender text name.
    
      * forward port to oreo
      * oreo edits:
        follow bool for OverflowNumbers as well
        before: https://i.imgur.com/JqeLFD1.jpg
        after: https://i.imgur.com/GPoM0zo.jpg
    
        Change-Id: I3c7828118991ec4fc616011caf073c81f75428b4
    ````````````````````````````````````````````````````````
    
      SubstratumResources/platform_frameworks_base@4b85726
      Author: George G <kreach3r@users.noreply.github.com>
      Date: Mon, 14 Nov 2016 14:49:47 +0200
      Commit: Themes: Notification dynamic colors bool compatible with OMS7
    
      * OMS7 introduced this fine piece of code:
        https://github.com/SubstratumResources/platform_frameworks_base/blob/n-oms7/core/java/android/app/ResourcesManager.java#L897..#L904
        // Resources.getSystem Resources are created on request and aren't tracked by
        // mResourceReferences.
        //
        // If overlays targeting "android" are to be used, we must create the system
        // resources regardless of whether they already exist, since otherwise the
        // information on what overlays to use would be lost. This is wasteful for most
        // applications, so limit this operation to the system user only. (This means
        // Resources.getSystem() will *not* use overlays for applications.)
    
      * Replaced deprecated Resources.getSystem() with compatible method.
    
      * forward ported to oreo
    
        Change-Id: I02efe27de3cc7067552964ffbaf079f9e9b5bc3e
    ````````````````````````````````````````````````````````
    
      SubstratumResources/platform_frameworks_base@79a8196
      Author: Adarsh-MR <adarshmr1998@gmail.com>
      Date: Mon, 27 Nov 2017 11:23:45 +0000
      Commit: Themes: Notifications: extend dynamic notification bool forambient notifications
    
      * added a seperate color resource, so this way themer can use diff color (ex: white)
        for ambient and accent for normal notification.
    
        Change-Id: I65a518d9433558004289826abbf25dc374dd7ec7
        Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>
    ````````````````````````````````````````````````````````
    
    Co-authored-by: George G <kreach3r@users.noreply.github.com>
    Co-authored-by: Adarsh-MR <adarshmr1998@gmail.com>
    
    Change-Id: I3c7828118991ec4fc616011caf073c81f75428b4
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -5469,6 +5469,9 @@ public class Notification implements Parcelable
         }
 
         int resolveContrastColor() {
+            if (!mContext.getResources().getBoolean(R.bool.config_allowNotificationIconTinting)) {
+                return mContext.getColor(R.color.notification_icon_default_color);
+            }
             if (mCachedContrastColorIsFor == mN.color && mCachedContrastColor != COLOR_INVALID) {
                 return mCachedContrastColor;
             }
@@ -5506,6 +5509,9 @@ public class Notification implements Parcelable
         }
 
         int resolveAmbientColor() {
+            if (!mContext.getResources().getBoolean(R.bool.config_allowNotificationIconTinting)) {
+                return mContext.getColor(R.color.notification_ambient_icon_default_color);
+            }
             if (mCachedAmbientColorIsFor == mN.color && mCachedAmbientColorIsFor != COLOR_INVALID) {
                 return mCachedAmbientColor;
             }
