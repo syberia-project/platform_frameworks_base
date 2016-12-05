00a0e5dae8dbfcf3cdd14ea1a8cc7d1655a87425
PMS22 <prathams99@rediff.com>
2016-12-05 15:38:36 +0000

    Launch default music player on headset connect [1/2]
    This commit squashes:
    	ffcfe4c AudioService: launch default music player on headset connect [2/3]
    	291b391 Do not start music app when headset is unplugged
    	f87c550 AudioService: don't launch music player while in call
    	95a503c AudioService: Prevent crash when there is no music app
    
    Change-Id: I4a8a31aadb2fa31db68015097676f93a4fe401a9
    Signed-off-by: PMS22 <prathams99@rediff.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4466,6 +4466,12 @@ public final class Settings {
         public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
 
         /**
+         * Whether to launch default music player when headset plugged in
+         * @hide
+         */
+        public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
