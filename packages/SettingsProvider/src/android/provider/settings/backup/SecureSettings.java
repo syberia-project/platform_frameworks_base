917a458e0e99bb57a949b3420d4ff49b259a35fd
maxwen <max.weninger@gmail.com>
2017-08-27 18:37:00 -0700

    Global Actions with Advanced Reboot [1/3]
    Vectorized the power menu sound panel selector
     -David Wilson <daveyannihilation@hotmail.com
    
    Pimp out power menu
    -Alex Cruz <mazdarider23@gmail.com>
    
    Powermenu Torch option
    -LorDClockaN <davor@losinj.com>
    
    Power menu customizations [1/2]
    -Dave Kessler <activethrasher00@gmail.com>
    
    Change-Id: I6978be623b0ec7ca9e702fc311449c36b504b1a6
    [jaysonedson: Updated for android11/12. Added setting
       for number of colums and rows to show. Adjusts for
       reboot menu.]
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -216,5 +216,6 @@ public class SecureSettings {
         Settings.Secure.BLUETOOTH_LE_BROADCAST_PROGRAM_INFO,
         Settings.Secure.BLUETOOTH_LE_BROADCAST_CODE,
         Settings.Secure.BLUETOOTH_LE_BROADCAST_APP_SOURCE_NAME
+        Settings.Secure.ADVANCED_REBOOT
     };
 }
