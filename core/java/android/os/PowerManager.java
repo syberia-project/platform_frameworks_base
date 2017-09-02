e7eaf206d9ecd619d48da70f05936e7129039075
maxwen <max.weninger@gmail.com>
2017-09-02 14:27:50 +0800

    WakelockBlocker [1/2] ported by @faust93
    Change-Id: I7bc63dc499e13ae00cc4856e282945c6df4c9db4
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- core/java/android/os/PowerManager.java
+++ core/java/android/os/PowerManager.java
@@ -1724,4 +1724,17 @@ public final class PowerManager {
             };
         }
     }
+
+    /**
+     * @hide
+     */
+    public String getSeenWakeLocks() {
+        try {
+            if (mService != null) {
+                return mService.getSeenWakeLocks();
+            }
+        } catch (RemoteException e) {
+        }
+        return null;
+    }
 }
