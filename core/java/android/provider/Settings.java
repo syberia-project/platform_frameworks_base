3809dcea2f0685fe01bac43bf140c009b5315f6e
ezio84 <brabus84@gmail.com>
2017-09-06 21:32:08 +0200

    QS: Add partial/full screenshot QS tile
    single tap to choose the mode, long press to fire
    
    thanks kantjer for the icons!
    
      From: maxwen <max.weninger@gmail.com>
      Date: Mon, 8 Jan 2018 02:52:58 +0100
      Subject: base: persist screenshot type in settings
    
      save state set in qs tile into settings
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Change-Id: I1ed62aaf4d41d8257ba8ef483a0b2debbcf063b0
    Signed-off-by: blinoff82 <blinov.in@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5171,6 +5171,13 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
 
         /**
+         * 0 - fullscreen
+         * 1 - partial
+         * @hide
+         */
+        public static final String SCREENSHOT_DEFAULT_MODE = "screenshot_default_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -43,6 +43,7 @@ import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WeatherTile;
@@ -188,6 +189,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mReadingModeTileProvider.get();
             case "weather":
                 return mWeatherTileProvider.get();
+            case "screenshot":
+                return new ScreenshotTile(mHost);
         }
 
         // Intent tiles.
