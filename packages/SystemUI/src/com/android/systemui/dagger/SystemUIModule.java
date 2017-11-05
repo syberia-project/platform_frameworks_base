fad9b0c07cd2c309e4a9adb2c793ad6fa5ce3de2
Steve Kondik <steve@cyngn.com>
2017-11-05 16:37:52 +0100

    SystemUI: Add LiveDisplay tile
    Author: Steve Kondik <steve@cyngn.com>
    Date:   Sun Nov 5 16:37:52 2017 +0100
    
        SystemUI: LiveDisplay tile
    
        Change-Id: I53d492e5cb9998268104d5750705aa5ed55d9658
        Signed-off-by: Joey Rizzoli <joey@lineageos.org>
    
    Author: Unpublished <unpublished@gmx.net>
    Date:   Fri Dec 29 23:00:01 2017 +0100
    
        livedisplay: Allow tile creation before boot completed phase [2/2]
    
        Change-Id: Ibef44d4e07da9baf296796515288c9d42aba8608
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Thu Feb 1 16:39:16 2018 +0000
    Edit:   Refactor for Oreo
    
    Change-Id: Ibef44d4e07da9baf296796515288c9d42aba8608
    
    Author: Bruno Martins <bgcngm@gmail.com>
    Date:   Thu Feb 1 17:01:43 2018 +0000
    
        LiveDisplayTile: Avoid NPE during boot up phase
    
         * During boot, with LineageHW not yet initialized there is no way to
           check the current LiveDisplay mode. In such cases, fallback to AUTO.
    
        Change-Id: Ifc004c86dfd68f73b8a362a1c833cbc3e7397811
    
    Author: Han Wang <416810799@qq.com>
    Date:   Tue Jun 4 18:03:59 2019 +0200
    
        LiveDisplayTile: Report unavailable on HWC2
    
        Change-Id: Ic10381e25046a2338bc6bda36ecbb5d5cefc1ab8
    
    Change-Id: I188df4f401e506db355ad8bb4b5307a8d0c3c35b
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
@@ -72,6 +72,7 @@ import com.android.systemui.plugins.BcSmartspaceConfigPlugin;
 import com.android.systemui.plugins.BcSmartspaceDataPlugin;
 import com.android.systemui.privacy.PrivacyModule;
 import com.android.systemui.process.condition.SystemProcessCondition;
+import com.android.systemui.syberia.SyberiaModule;
 import com.android.systemui.qrcodescanner.dagger.QRCodeScannerModule;
 import com.android.systemui.qs.FgsManagerController;
 import com.android.systemui.qs.FgsManagerControllerImpl;
@@ -215,6 +216,7 @@ import javax.inject.Named;
             SecurityRepositoryModule.class,
             ScreenRecordModule.class,
             SettingsUtilModule.class,
+            SyberiaModule.class,
             SmartRepliesInflationModule.class,
             SmartspaceModule.class,
             StatusBarPipelineModule.class,
