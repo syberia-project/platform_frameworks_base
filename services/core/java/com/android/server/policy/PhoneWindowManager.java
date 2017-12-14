cba697d53809bed921f4e29f2064938060af738f
DennySPB <dennyspb@gmail.com>
2017-12-14 15:22:42 +0300

    Enable HOME as wake key

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6358,6 +6358,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 }
                 break;
             }
+		case KeyEvent.KEYCODE_HOME:
+                if (down && !interactive) {
+                    isWakeKey = true;
+                }
+                break;
 
             case KeyEvent.KEYCODE_ENDCALL: {
                 result &= ~ACTION_PASS_TO_USER;
