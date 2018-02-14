ede03e88523ca3b9a191237728c572f194dfa64f
Varun Garg <gargv@codeaurora.org>
2018-02-15 05:08:16 +0530

    BoostFramework: To Enhance performance.
    The following functionalities are sqaushed in this change
    
    1. To enhance performance during critical scenarios
    
    Adding Boost APIs which can ensure the best possible resource
    availability during critical UX scenarios. These APIs can be
    either turned off or dynamically hooked to OEM specific
    performance optimizations.
    
    2. Avoid class loading from QPerformance jar
    
    QPerformance jar is part of PRODUCT_BOOT_JARS and the required
    class is already available in zygote. Hence, class loading
    can be avoided.
    
    3. Expose perfLock handle to JAVA context.
    
    When user calls perfLockAcquire/perfHint, if SUCCEED, the return value
    is unique handle,which is > 0, if FAILED, the return value is -1.
    perfLockReleaseHandler API is added, to help the user release perflock
    in different contexts.
    
    public int perfLockReleaseHandler(int handle), handle is the unique value.
    
    4. Detect scrolls and boost frequencies
    
       Detect scroll and boost frequencies. Perf hint of vertical
       scrolls is used for scrolls
    
    5. Add the perfHint ID for MTP file copy
    
    6. Introduce perf hints for prekill feature.
    
    Introduce perf hints during service start, restart,
    broadcasts & content providers & first frame draw.
    
    7. Touch tap hint support after first launch
    
    Supporting tap hints after first launch of apps
    
    Squash of following change-IDs:
           Change-Id: I2a363396e532c52287156ae9c6dfd81e740e92f1
           Change-Id: Id0a611e0f2415e1621d7d0a4988d75cd2186a672
           Change-Id: I48a2513c524c1fa69609cc405940a44590243fa7
           Change-Id: I32a51877222f34660d021ced2ed31a392926a15e
           Change-Id: I7610f247d5294f4f9f3f18a6813ac818d7f0659d
           Change-Id: I577fed178be07fb74bc861ae67e586c7c936dc03
           Change-Id: Iffe8ec81cc24297b380a3cb628227ece302a0953
    
    CRs-Fixed: 2055079 2071185 2017684 2114569 2140194 2088484 2142307
    
    Change-Id: Ib7da4c06f53a3809a01e320c7e6cc6b9c29ce462
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- services/core/java/com/android/server/am/ActivityRecord.java
+++ services/core/java/com/android/server/am/ActivityRecord.java
@@ -205,6 +205,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import android.util.BoostFramework;
 
 /**
  * An entry in the history stack, representing an activity.
@@ -363,6 +364,7 @@ final class ActivityRecord extends ConfigurationContainer implements AppWindowCo
 
     private boolean mShowWhenLocked;
     private boolean mTurnScreenOn;
+    public static BoostFramework mPerfFirstDraw = null;
 
     /**
      * Temp configs used in {@link #ensureActivityConfiguration(int, boolean)}
@@ -2090,6 +2092,12 @@ final class ActivityRecord extends ConfigurationContainer implements AppWindowCo
             Log.i(TAG, sb.toString());
         }
         mStackSupervisor.reportActivityLaunchedLocked(false, this, thisTime, totalTime);
+        if (mPerfFirstDraw == null) {
+            mPerfFirstDraw = new BoostFramework();
+        }
+        if (mPerfFirstDraw != null) {
+            mPerfFirstDraw.perfHint(BoostFramework.VENDOR_HINT_FIRST_DRAW, info.packageName, (int)thisTime, BoostFramework.Draw.EVENT_TYPE_V1);
+        }
         if (totalTime > 0) {
             //service.mUsageStatsService.noteLaunchTime(realActivity, (int)totalTime);
         }
