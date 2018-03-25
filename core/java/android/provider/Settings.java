ad622509af2df85d11758381ed386b6ddf3e1dd5
maxwen <max.weninger@gmail.com>
2018-03-26 01:02:25 +0200

    base: Implement animations for back edge gestures [1/2]
    Change-Id: I49765840ecd02ea5decc3ce602ff2778ee99f117
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9796,6 +9796,18 @@ public final class Settings {
                 "status_bar_locked_on_secure_keyguard";
 
         /**
+         * Show UI feedback when using back gesture.
+         * @hide
+         */
+        public static final String EDGE_GESTURES_BACK_SHOW_UI_FEEDBACK = "edge_gestures_back_show_ui_feedback";
+
+        /**
+         * Use black arrow theme instead of the white version.
+         * @hide
+         */
+        public static final String EDGE_GESTURES_BACK_USE_BLACK_ARROW = "edge_gestures_back_use_black_arrow";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
