15598fa06901e148e59f0f16c56e6b1d3734d47f
maxwen <max.weninger@gmail.com>
2018-05-23 21:55:04 +0200

    base: SystemUI: prevent that double tap to sleep triggers panel expand
    http://jira.omnirom.org/browse/OMNI-1466
    
    Change-Id: If304577d666ebf8423cca1298dfbe044843657cc
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -566,6 +566,11 @@ public class NotificationPanelViewController extends PanelViewController {
             @Override
             public boolean onDoubleTap(MotionEvent e) {
                 PowerUtils.switchScreenOff(mView.getContext());
+                // quick pulldown can trigger those values
+                // on double tap - so reset them
+                mQsExpandImmediate = false;
+                requestPanelHeightUpdate();
+                setListening(false);
                 return true;
             }
         });
