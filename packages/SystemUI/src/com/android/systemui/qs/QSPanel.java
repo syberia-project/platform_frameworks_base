4daa8805f0dbc57c9b036a4718624bda9f439c41
Alex <alex9yust@gmail.com>
2018-06-18 21:41:01 +0300

    SystemUI: fix the behavior of QS with isDualTarget=true
    Change-Id: I0c5b3c7165547b9b50bec414f2adc469b362d420
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: blinoff82 <blinov.in@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -721,7 +721,7 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
                     setAnimationTile(v);
             });
             if (t.isDualTarget()) {
-                if (!mTileLayout.isShowTitles()) {
+                if (mTileLayout.isShowTitles()) {
                     v.setOnLongClickListener(view -> {
                         t.secondaryClick();
                         return true;
