55caddff98cc0e52302377ca14c1efb9558a62a5
Ded_Boi <dhanush911gtr@gmail.com>
2018-10-09 19:35:09 +0000

    Base: Update statusbar NFC icon drawable
    Change-Id: Id4de96e3fb79be8612496d18bd3427152efb2ff3
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tuner/StatusbarItems.java
+++ packages/SystemUI/src/com/android/systemui/tuner/StatusbarItems.java
@@ -15,11 +15,14 @@
 package com.android.systemui.tuner;
 
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.preference.Preference;
 import android.support.v14.preference.SwitchPreference;
 import android.support.v14.preference.PreferenceFragment;
+import android.support.v7.preference.PreferenceScreen;
 import com.android.systemui.R;
 
 public class StatusbarItems extends PreferenceFragment {
@@ -33,11 +36,13 @@ public class StatusbarItems extends PreferenceFragment {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final PackageManager pm = getActivity().getApplicationContext().getPackageManager();
         ContentResolver resolver = getActivity().getContentResolver();
 
         mShowSyberiaLogo = (SwitchPreference) findPreference(KEY_STATUS_BAR_LOGO);
         mShowSyberiaLogo.setChecked((Settings.System.getInt(resolver, Settings.System.STATUS_BAR_LOGO, 1) == 1));
+
 	mNfcSwitch = (StatusBarSwitch) findPreference(NFC_KEY);
 
         final boolean isNfcAvailable = pm.hasSystemFeature(PackageManager.FEATURE_NFC);
