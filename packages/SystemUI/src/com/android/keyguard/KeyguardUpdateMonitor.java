ca1fa3af0b94e56adac9b94aab846482cf9d860e
DennySPB <dennyspb@gmail.com>
2018-10-22 12:33:04 +0300

    Merge tag 'android-9.0.0_r12'
    Android 9.0.0 release 12
    
    Change-Id: I65be58f9aa956deb1566f41a80dfcff76632baea
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1669,6 +1669,10 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
         }
     }
 
+    public boolean isKeyguardVisible() {
+        return mKeyguardIsVisible;
+    }
+
     /**
      * Notifies that the visibility state of Keyguard has changed.
      *
