e1a8746db6782920ded694976716135a4af212d8
bigrushdog <randall.rushing@gmail.com>
2018-11-21 20:03:32 +0100

    bringup: Fontservice
    Change-Id: I063528aa02d43904c49e02b8f832391fad793171
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5172,6 +5172,20 @@ public final class Settings {
         public static final String DOT_COLOR_SWITCH = "dot_color_switch";
 
         /**
+         * Hold current font name and name of the originating package Uses "|" as a delimiter
+         *
+         * @hide
+         */
+        public static final String FONT_INFO = "font_info";
+        
+        /**
+         * Hold list of installed font provider packages. Uses "|" as a delimiter
+         *
+         * @hide
+         */
+        public static final String FONT_PACKAGES = "font_packages";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -235,6 +235,8 @@ public final class SystemServer {
             "com.android.internal.car.CarServiceHelperService";
     private static final String PERF_SERVICE_CLASS =
             "com.qualcomm.qti.PerfService";
+    private static final String FONT_SERVICE_CLASS =
+            "com.android.server.FontService$Lifecycle";
 
     private static final String PERSISTENT_DATA_BLOCK_PROP = "ro.frp.pst";
 
@@ -704,6 +706,11 @@ public final class SystemServer {
             startSensorService();
             traceLog.traceEnd();
         }, START_SENSOR_SERVICE);
+	
+	    // Manages fonts
+        traceBeginAndSlog("StartFontService");
+        mSystemServiceManager.startService(FONT_SERVICE_CLASS);
+        traceEnd();
     }
 
     /**
