c773c62f28917a0683479fbefea2524b4dd02756
SpiritCroc <dev@spiritcroc.de>
2018-12-24 15:03:44 +0100

    Network traffic: fix indicator not hiding on lost connection
    With https://gerrit.aicp-rom.com/c/AICP/frameworks_base/+/80636
    ("Statusbar net monitor: stop the handler if screen is off"),
    connectivity status can change without us noticing, so let's update in
    updateSettings() (which is called after turning the screen on) also if
    there is no connection.
    
    Change-Id: I674f9cc3946f679b1ff9696079c5608fdfaa12d7
    Signed-off-by: blinoff82 <blinov.in@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
@@ -272,15 +272,13 @@ public class NetworkTraffic extends TextView implements DarkReceiver,StatusIconD
     private void updateSettings() {
         updateVisibility();
         if (mIsEnabled) {
-            if (getConnectAvailable()) {
-                if (mAttached) {
-                    totalRxBytes = TrafficStats.getTotalRxBytes();
-                    lastUpdateTime = SystemClock.elapsedRealtime();
-                    mTrafficHandler.sendEmptyMessage(1);
-                }
-                updateTrafficDrawable();
-                return;
+            if (mAttached) {
+                totalRxBytes = TrafficStats.getTotalRxBytes();
+                lastUpdateTime = SystemClock.elapsedRealtime();
+                mTrafficHandler.sendEmptyMessage(1);
             }
+            updateTrafficDrawable();
+            return;
         } else {
             clearHandlerCallbacks();
         }
