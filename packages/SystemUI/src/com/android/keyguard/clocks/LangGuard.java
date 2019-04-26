b68e1b373daca2d16d21128ce83b4b17eea0de49
DennySPB <dennyspb@gmail.com>
2019-04-26 14:15:26 +0300

    Q Clock: add RU translation
    Change-Id: Idbdef34c299da35d7e4e70cf20cdf28822216959
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/clocks/LangGuard.java
+++ packages/SystemUI/src/com/android/keyguard/clocks/LangGuard.java
@@ -35,7 +35,7 @@ public class LangGuard {
                 if (units == 1) {
                     numString = TensString[tens].substring(0, TensString[tens].length() - 1)+
                                 UnitsString[units].toLowerCase();
-                    return numString;                    
+                    return numString;
                 } else if (units == 3) {
                     numString = TensString[tens] + "tré";
                     return numString; 
@@ -48,12 +48,17 @@ public class LangGuard {
                 numString = TensString[tens] + "e " + UnitsString[units].toLowerCase();
                 return numString;
 
-            case "fr":
+            case "ru":
                 if (units == 1) {
-                    numString = TensString[tens] + "et un";
+                    numString = TensString[tens].substring(0, TensString[tens].length() - 1)+
+                                UnitsString[units];
+                    return numString;
                 } else {
-                    numString = TensString[tens] + UnitsString[units].toLowerCase();
+                    numString = TensString[tens] +" "+ UnitsString[units];
+                    return numString;
                 }
+
+            case "fr":
                 return numString;
         }
         return numString;
