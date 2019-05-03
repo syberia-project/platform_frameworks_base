658e08d6165d15ca788b82d241583dfecf3b25a0
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-03 16:49:15 -0300

    Introduce LiveDisplay from Lineage
    All credits to LineageOS and related authors on:
    
    https://github.com/LineageOS/android_lineage-sdk
    
    Included fwb commit:
    
    commit 5ef7a9bb0996cc0c141d054514744ceaa93ba036
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Tue Feb 20 07:51:43 2018 -0600
    
        NightDisplayController: report unavailable if livedisplay feature is present
    
        Change-Id: Id677e76d452ced2e62373337d63593c7b85f2fba
    
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I35bf801aca2e31f30e9d1a0488648f93eab84492

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -146,6 +146,10 @@ import static android.os.IServiceManager.DUMP_FLAG_PRIORITY_NORMAL;
 import static android.os.IServiceManager.DUMP_FLAG_PROTO;
 import static android.view.Display.DEFAULT_DISPLAY;
 
+// LiveDisplay
+import com.android.server.custom.LineageHardwareService;
+import com.android.server.custom.display.LiveDisplayService;
+
 public final class SystemServer {
     private static final String TAG = "SystemServer";
 
@@ -1638,6 +1642,13 @@ public final class SystemServer {
 
             traceBeginAndSlog("Starting ScreenStabilization Service");
             mSystemServiceManager.startService(ScreenStabilization.class);
+
+            // LiveDisplay
+            traceBeginAndSlog("StartLineageHardwareService");
+            mSystemServiceManager.startService(LineageHardwareService.class);
+            traceEnd();
+            traceBeginAndSlog("StartLiveDisplayService");
+            mSystemServiceManager.startService(LiveDisplayService.class);
             traceEnd();
         }
 
