24a8ad919100273a2f6ebe26609a462227e8588e
bigrushdog <randall.rushing@gmail.com>
2019-05-06 10:16:07 -0700

    SmartNav: Eliminate needless BroadcastReceiver in BaseNavigationBar [1/2]
    This fixes Pulse not running after unlock from fingerprint. This also
    fixes Pulse not running when changing from another bar implementation
    into stock navbar. There will be MUCH MORE work with PulseController
    but this is good for now.
    
    Change-Id: I90cad8582f4ee5b3583cd20194c193420bcc1379
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -1095,13 +1095,6 @@ public class NavigationBarView extends FrameLayout implements Navigator, PulseOb
     }
 
     @Override
-    public final void notifyInflateFromUser() {
-        if (mPulse != null) {
-            mPulse.notifyScreenOn(true);
-        }
-    }
-
-    @Override
     public void setLeftInLandscape(boolean leftInLandscape) {
         if (mPulse != null) {
             mPulse.setLeftInLandscape(leftInLandscape);
