5bcd46e7989707d540e2c20e9c5c08d20b4afac2
Manisha Agarwal <maniagar@codeaurora.org>
2019-08-17 10:49:32 +0530

    Audio: Toggle unmute mute during SHO.
    Mute and unmute during SHO to avoid audio big sound
    volume issue.
    
    CRs-Fixed: 2503819
    Change-Id: I083ce600a3450b7a99c5d44ab08517ad8e06e71f
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- services/core/java/com/android/server/audio/AudioDeviceInventory.java
+++ services/core/java/com/android/server/audio/AudioDeviceInventory.java
@@ -319,8 +319,15 @@ public class AudioDeviceInventory {
                 return;
             }
 
+            boolean wasMuted = false;
+
             if (event == BtHelper.EVENT_ACTIVE_DEVICE_CHANGE) {
                 // Device is connected
+
+                Log.i(TAG, "Mute the stream before reconfigure");
+                mDeviceBroker.postAccessoryPlugMediaMute(AudioSystem.DEVICE_OUT_BLUETOOTH_A2DP);
+                wasMuted = true;
+
                 if (a2dpVolume != -1) {
                     mDeviceBroker.postSetVolumeIndexOnDevice(AudioSystem.STREAM_MUSIC,
                             // convert index to internal representation in VolumeStreamState
@@ -343,6 +350,11 @@ public class AudioDeviceInventory {
                         btDevice, BluetoothA2dp.STATE_DISCONNECTED, BluetoothProfile.A2DP,
                         false /* suppressNoisyIntent */, musicDevice,
                         -1 /* a2dpVolume */);
+            }
+
+            if (wasMuted && event == BtHelper.EVENT_ACTIVE_DEVICE_CHANGE) {
+                Log.i(TAG, "Unmute the stream after reconfigure");
+                mDeviceBroker.postAccessoryPlugMediaUnmute(AudioSystem.DEVICE_OUT_BLUETOOTH_A2DP);
             }
         }
     }
