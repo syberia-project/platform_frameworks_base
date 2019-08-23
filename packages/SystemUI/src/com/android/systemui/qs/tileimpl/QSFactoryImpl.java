0dd59b67f9cd4bcb369effbb129803b3b19f7432
DennySPB <dennyspb@gmail.com>
2019-08-23 13:09:27 +0300

    Drop screenrecord
    Revert "ScreenRecord: respect selected quality using screenrecord chord"
    
    This reverts commit fa15c843cdbf99e0ab3a9e6b1ad4a2ed96487cd9.
    
    Change-Id: I3b1bb8a0ab0de4273d441ff7e79a11feb408de2e
    
    Revert "base: screenrecord: update notif text for high aspect ratio devices"
    
    This reverts commit 9db3237b8a0d7ffaebef6fb6251bf5bde28d3cc2.
    
    Change-Id: I9d223cff257969c465d0e8cefb0602d93bd30b10
    
    Revert "Screenrecord: removed scanning the tmp file"
    
    This reverts commit f11a56f4e887487476e941e3dc28f94e2a130ad4.
    
    Change-Id: Idae52a7194bc566f9456e909e35a33ef5a34b199
    
    Revert "screenrecord: add support for 18:9 resolutions"
    
    This reverts commit 421ebcae564ee3e5eec9a740ac85749c90647a1a.
    
    Change-Id: Ia7d8518a574c0068b0c610000e0d2fbd904f975a
    
    Revert "Screenrecord: Adjust 'High' setting to 1080p"
    
    This reverts commit d50936cd10395d60593a4e9d15e7aa622618aeb7.
    
    Change-Id: I16b5e32ecd768ddb29e5f5b0607ccd460a7763ef
    
    Revert "Screenrecord tile: save quality level"
    
    This reverts commit fdd481d1a01a59c2f137bef0c40e50a0c50bc29b.
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: Iaee1eb36451f9e5d587c15421837c908dae6303f
    
    Revert "QS: add Screenrecord tile"
    
    This reverts commit 05fe14ecd6ecaaf373813554c4c24682afd2fea8.
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I9f00bfc7cce8cd7e9f5c4ce8949c0040e36a2bf4
    
    Revert "Squashed commit of the following:"
    
    This reverts commit ed08ae2303a1edd8de68abcbb5a0f052cd0a571a.
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I82e5e213d4ca3e8777273c375b2aedfa1ea1a84a
    
    Revert "Multi ScreenRecord tile, blinking stop button, screenrecord service improvements [1/2]"
    
    This reverts commit 65ea4ba4281e94ef33ddffdb9a78decbd08fdf67.
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I3a5993f0b01deff31bb5b2396a9272323f5697a5
    
    Revert "Save screenrecord in "Screenrecords" directory"
    
    This reverts commit f5ea6771db65b59320a7b9493012547fda21f502.
    
    Change-Id: I4e5bf5fd1d5559b6c37f3622ffa922691072d060
    
    Revert "Add a timer to screen record"
    
    This reverts commit 4f1c314860e48add749b46a2f47f8eb393744d76.
    
    Change-Id: Ia603af8f7371b35212f30a47bcbf8b2f66f78949
    
    Revert "Base: bring back screenrecord"
    
    This reverts commit 61c363eb882a7b5422b4e402917500ded5c432eb.
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: Ib79f6832b8ba623ee5122699b110ac334f7203b2
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -52,7 +52,6 @@ import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.PieTile;
 import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
-import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.ScreenStabilizationTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
@@ -123,8 +122,6 @@ public class QSFactoryImpl implements QSFactory {
                 return new NfcTile(mHost);
             case "lte":
                 return new LteTile(mHost);
-            case "screenrecord":
-                return new ScreenrecordTile(mHost);
             case "caffeine":
                 return new CaffeineTile(mHost);
             case "sound":
