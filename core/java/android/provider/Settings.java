eb9dd60b435fa33b51b2a669c7f52563381cc76e
SKULSHADY <anushekprasal@gmail.com>
2019-10-10 15:39:20 +0200

    base: Double tap to trigger doze [1/2]
    Thanks to yank555-lu for the original commit YAOSP-NG/frameworks_base@d6ac164.
    Cleaned up and ported to Android 10 by SKULSHADY.
    
    Change-Id: I4154925a0c7de37f1dc84b58b280691cc44d14c9
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5650,7 +5650,7 @@ public final class Settings {
         public static final String TOAST_ICON = "toast_icon";
 
          /**
-	 * FOD icon picker
+         * FOD icon picker
          * @hide
          */
         public static final String FOD_ICON = "fod_icon";
@@ -5666,6 +5666,12 @@ public final class Settings {
          * @hide
          */
         public static final String FOD_PRESSED_STATE = "fod_pressed_state";
+
+        /**
+         * Use doubletap as doze pulse triggers
+         * @hide
+         */
+        public static final String DOZE_TRIGGER_DOUBLETAP = "doze_trigger_doubletap";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
