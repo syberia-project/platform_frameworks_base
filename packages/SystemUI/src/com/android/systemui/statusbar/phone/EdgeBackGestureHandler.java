74552ed4c6cac95340d1d341a6e099146cc59801
DennySPB <dennyspb@gmail.com>
2019-10-11 15:51:47 +0300

    SystemUI: Fix navbar toggle
    Syberia: also allow using edge gestures with navbar disabled
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I9959a72806bc2a5df27813e5d08a0c0bec6ff630

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -226,7 +226,7 @@ public class EdgeBackGestureHandler implements DisplayListener {
     }
 
     private void updateIsEnabled() {
-        boolean isEnabled = mIsAttached && mIsGesturalModeEnabled;
+        boolean isEnabled = mIsGesturalModeEnabled;
         if (isEnabled == mIsEnabled) {
             return;
         }
