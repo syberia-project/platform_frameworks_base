013f7d3cc2652ba60ca0d40330a83fc82585a0cf
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2019-10-14 21:07:38 +0530

    StatusbarClock: date right/left position [1/2]
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: Ib2a39076b02c417f3ad3ff3d1ae555d68a90fc06

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5821,6 +5821,14 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_DATE_FORMAT = "statusbar_clock_date_format";
 
         /**
+          * Position of date
+          * 0 - Left of clock
+          * 1 - Right of clock
+          * @hide
+          */
+         public static final String STATUSBAR_CLOCK_DATE_POSITION = "statusbar_clock_date_position";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -6012,6 +6020,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_DATE_DISPLAY);
             PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_DATE_STYLE);
             PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_DATE_FORMAT);
+            PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_DATE_POSITION);
         }
 
         /**
