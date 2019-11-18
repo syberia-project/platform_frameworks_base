beb8b472cfef38cbcd3fd9f38ada7a324bfb0e20
ezio84 <brabus84@gmail.com>
2019-11-18 22:56:47 +0100

    Losckreen blur level: code fixes
    check if blur_radius < 1 before checking if it's <5
    
    use existing statusbar settings observer to avoid too many settings
    provider calls when triggering processArtwork (it's called also on
    wake events when goint to lockscreen)
    
    set the default system setting value to 2500,
    because then we divide it for 100 to get the stock default value
    that was 25 in MediaArtwork class.
    Remember to set android:defaultValue to 2500 for the seekbar in
    the Settings part of this commit.
    
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Change-Id: Ieb71b835228e985b1b29b7c9698166dd47be7e95
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4673,6 +4673,9 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.DOUBLE_TAP_SLEEP_GESTURE),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_MEDIA_BLUR),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -4684,17 +4687,27 @@ public class StatusBar extends SystemUI implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.DOUBLE_TAP_SLEEP_GESTURE))) {
                 setLockscreenDoubleTapToSleep();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_MEDIA_BLUR))) {
+                setLockScreenMediaBlurLevel();
             }
         }
 
         public void update() {
             setLockscreenDoubleTapToSleep();
+            setLockScreenMediaBlurLevel();
         }
     }
 
     private void setLockscreenDoubleTapToSleep() {
         if (mNotificationShadeWindowViewController != null) {
             mNotificationShadeWindowViewController.setLockscreenDoubleTapToSleep();
+        }
+    }
+
+    private void setLockScreenMediaBlurLevel() {
+        if (mMediaManager != null) {
+            mMediaManager.setLockScreenMediaBlurLevel();
         }
     }
 
