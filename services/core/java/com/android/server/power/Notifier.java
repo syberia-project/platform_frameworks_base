a24855856ca0d8989e6450234d9c048c4f549e55
DennySPB <dennyspb@gmail.com>
2019-11-27 10:38:10 +0300

    Charging notifier: don't force play sound on plug in
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: If28d720ebcf67ee4243767bb7c080169798e35f3
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- services/core/java/com/android/server/power/Notifier.java
+++ services/core/java/com/android/server/power/Notifier.java
@@ -822,7 +822,6 @@ public class Notifier {
     private void showWiredChargingStarted(@UserIdInt int userId,int batteryLevel) {
         final boolean animationEnabled = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.CHARGING_ANIMATION, 1) == 1;
-        playChargingStartedVibration(userId);
         playChargingStartedFeedback(userId);
         if (mStatusBarManagerInternal != null && animationEnabled) {
             mStatusBarManagerInternal.showChargingAnimation(batteryLevel);
