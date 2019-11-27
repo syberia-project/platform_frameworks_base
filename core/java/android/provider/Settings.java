ea3966583caa42c0dd0cba36b3029cc6b8f2b003
maxwen <max.weninger@gmail.com>
2019-11-27 16:27:40 +0100

    SystemUI: old style mobile data indicators [1/2]
    neobuddy89: Use Tuner API.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Change-Id: I2fbf10a127993ba4ee28bc5a71a32550329b9c53

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5869,6 +5869,11 @@ public final class Settings {
          */
         public static final String LOCK_POWER_MENU_DISABLED = "lockscreen_power_menu_disabled";
 
+	/**
+         * @hide
+         */
+        public static final String USE_OLD_MOBILETYPE = "use_old_mobiletype";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
