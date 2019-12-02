7751f0fd260cb9de31105dda90afa352d2b75ebd
ProchyGaming <mcmovieslovakiasro@gmail.com>
2019-12-02 19:16:57 +0100

    Open Sound settings on long click at the panel button
    @StarkDroid: Adapt for 12.1
    
    Signed-off-by: Trishiraj <gamerprince.exp@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Ida3a634a12f5d0bd0c67263967bb9bc378ef8ea1

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -101,6 +101,7 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityManager;
+import android.view.View.OnLongClickListener;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.FrameLayout;
@@ -147,7 +148,7 @@ import java.util.function.Consumer;
  */
 public class VolumeDialogImpl implements VolumeDialog,
         ConfigurationController.ConfigurationListener,
-        ViewTreeObserver.OnComputeInternalInsetsListener {
+        ViewTreeObserver.OnComputeInternalInsetsListener, OnLongClickListener {
     private static final String TAG = Util.logTag(VolumeDialogImpl.class);
 
     private static final long USER_ATTEMPT_GRACE_PERIOD = 1000;
@@ -652,6 +653,7 @@ public class VolumeDialogImpl implements VolumeDialog,
 
         mSettingsView = mDialog.findViewById(R.id.settings_container);
         mSettingsIcon = mDialog.findViewById(R.id.settings);
+        mSettingsIcon.setOnLongClickListener(this);
 
         mExpandRowsView = mDialog.findViewById(R.id.expandable_indicator_container);
         mExpandRows = mDialog.findViewById(R.id.expandable_indicator);
@@ -2630,6 +2632,22 @@ public class VolumeDialogImpl implements VolumeDialog,
             }
             return false;
         }
+    }
+
+    public boolean onLongClick(View v) {
+        if (v == mSettingsIcon) {
+            startSoundActivity();
+        }
+        return false;
+    }
+
+    private void startSoundActivity() {
+        dismissH(Events.DISMISS_REASON_TOUCH_OUTSIDE);
+        Intent nIntent = new Intent(Intent.ACTION_MAIN);
+        nIntent.setClassName("com.android.settings",
+            "com.android.settings.Settings$SoundSettingsActivity");
+        mMediaOutputDialogFactory.dismiss();
+        mActivityStarter.startActivity(nIntent, true /* dismissShade */);
     }
 
     private final class VolumeSeekBarChangeListener implements OnSeekBarChangeListener {
