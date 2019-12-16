d9e30fefc4216695a14d753f894d511f2dd3fea2
00day0 <therandomuser11@gmail.com>
2019-12-16 21:31:27 +0100

    SystemUI: FOD icon picker [1/2]
    Co-authored-by: Stallix <thestallix@gmail.com>
    Change-Id: Ib280d9f36e4f4318ef9adfb232e9170bec881658

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5607,6 +5607,12 @@ public final class Settings {
         public static final String PULSE_ON_NEW_TRACKS = "pulse_on_new_tracks";
 
         /**
+         * FOD icon picker
+         * @hide
+         */
+        public static final String FOD_ICON = "fod_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
