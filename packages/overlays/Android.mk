e98f2d7aec204cccf9b7653fd83013d64990a244
blinoff82 <blinov.in@gmail.com>
2020-01-01 18:11:13 +0300

    base: Add some our fonts from Pie [1/2]
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Change-Id: I1ea530d5753ec7c9c2f6a303e8cd49167ec50a50
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -45,6 +45,13 @@ LOCAL_REQUIRED_MODULES := \
 	FontSurferSourceOverlay \
 	FontNokiaPureSourceOverlay \
 	FontFifa2018SourceOverlay \
+	FontOswaldSourceOverlay \
+	FontExo2SourceOverlay \
+	FontUbuntuRegularSourceOverlay \
+	FontRobotoCondensedLightSourceOverlay \
+	FontStoropiaSourceOverlay \
+	FontRoundedEleganceSourceOverlay \
+	FontCaviarDreamsSourceOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularLauncherOverlay \
 	IconPackCircularSettingsOverlay \
