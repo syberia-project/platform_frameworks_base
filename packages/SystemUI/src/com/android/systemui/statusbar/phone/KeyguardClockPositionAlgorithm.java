b896a894fdaea19360b96e448c832f9303598b26
DennySPB <dennyspb@gmail.com>
2020-01-23 09:06:07 +0300

    Revert "KeyguardClockPositionAlgorithm: Allow forcing default clockY positioning"
    This reverts commit 81d695be2280bfe12c778c19ee45e7cceef5d6e8.
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: I52a22c93ef5b0b88c95736c40348f71f83d5b9ea

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardClockPositionAlgorithm.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardClockPositionAlgorithm.java
@@ -39,11 +39,6 @@ public class KeyguardClockPositionAlgorithm {
     private static float CLOCK_HEIGHT_WEIGHT = 0.7f;
 
     /**
-     * Allow using the default clock Y calculations
-     */
-    public static int CLOCK_USE_DEFAULT_Y = -1;
-
-    /**
      * Margin between the bottom of the clock and the notification shade.
      */
     private int mClockNotificationsMargin;
@@ -185,17 +180,12 @@ public class KeyguardClockPositionAlgorithm {
         return mHeight / 2 - mKeyguardStatusHeight - mClockNotificationsMargin;
     }
 
-    private int getPreferredAlternativeClockY(int alternative) {
-        if (mClockPreferredY != CLOCK_USE_DEFAULT_Y) {
-            return mClockPreferredY;
-        } else  {
-            return alternative;
-        }
+    private int getPreferredClockY() {
+        return mClockPreferredY;
     }
 
     private int getExpandedPreferredClockY() {
-        return (mHasCustomClock && (!mHasVisibleNotifs || mBypassEnabled))
-                ? getPreferredAlternativeClockY(getExpandedClockPosition())
+        return (mHasCustomClock && (!mHasVisibleNotifs || mBypassEnabled)) ? getPreferredClockY()
                 : getExpandedClockPosition();
     }
 
@@ -225,8 +215,8 @@ public class KeyguardClockPositionAlgorithm {
 
     private int getClockY(float panelExpansion) {
         // Dark: Align the bottom edge of the clock at about half of the screen:
-        float clockYDark = (mHasCustomClock ? getPreferredAlternativeClockY(getMaxClockY())
-            : getMaxClockY()) + burnInPreventionOffsetY();
+        float clockYDark = (mHasCustomClock ? getPreferredClockY() : getMaxClockY())
+                + burnInPreventionOffsetY();
         clockYDark = MathUtils.max(0, clockYDark);
 
         float clockYRegular = getExpandedPreferredClockY();
