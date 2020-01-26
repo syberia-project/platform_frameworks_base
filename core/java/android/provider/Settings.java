26d69e3ca9196d55f17268f30f04da55d5312345
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-26 17:31:14 +0900

    base: Introduce adjustable blur intensity [1/2]
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Change-Id: I44268a22afb685edba471fc677e93ce7f66bb05c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5744,6 +5744,15 @@ public final class Settings {
         private static final Validator QS_BACKGROUND_BLUR_ALPHA_VALIDATOR = ANY_INTEGER_VALIDATOR;
 
         /**
+         * QS blur intensity
+         * @hide
+         */
+        public static final String QS_BACKGROUND_BLUR_INTENSITY = "qs_background_blur_intensity";
+
+        /** @hide */
+        private static final Validator QS_BACKGROUND_BLUR_INTENSITY_VALIDATOR = ANY_INTEGER_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -5879,6 +5888,7 @@ public final class Settings {
             PULSE_AMBIENT_LIGHT_REPEAT_COUNT,
             QS_BACKGROUND_BLUR,
             QS_BACKGROUND_BLUR_ALPHA,
+            QS_BACKGROUND_BLUR_INTENSITY,
         };
 
         /**
@@ -6277,6 +6287,7 @@ public final class Settings {
             VALIDATORS.put(PULSE_AMBIENT_LIGHT_REPEAT_COUNT, PULSE_AMBIENT_LIGHT_REPEAT_COUNT_VALIDATOR);
             VALIDATORS.put(QS_BACKGROUND_BLUR, QS_BACKGROUND_BLUR_VALIDATOR);
             VALIDATORS.put(QS_BACKGROUND_BLUR_ALPHA, QS_BACKGROUND_BLUR_ALPHA_VALIDATOR);
+            VALIDATORS.put(QS_BACKGROUND_BLUR_INTENSITY, QS_BACKGROUND_BLUR_INTENSITY_VALIDATOR);
         }
 
         /**
