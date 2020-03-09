38ae96014308f68b305adbef6a720316bef3a92e
blinoff82 <blinov.in@gmail.com>
2020-03-09 19:07:33 +0300

    base: New font-pack [1/2]
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Change-Id: I61bbaef2a002902f64ec9f97604eae0a9e533fec
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -53,6 +53,14 @@ LOCAL_REQUIRED_MODULES := \
 	FontRoundedEleganceSourceOverlay \
 	FontCaviarDreamsSourceOverlay \
 	FontSanFranciscoDisplayProSourceOverlay \
+	FontDomkratSourceOverlay \
+	FontFakedesSourceOverlay \
+	FontHondaSourceOverlay \
+	FontHortensiaSourceOverlay \
+	FontMinusmanSourceOverlay \
+	FontNovaSourceOverlay \
+	FontTaurusSourceOverlay \
+	FontTechnicalSourceOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularLauncherOverlay \
 	IconPackCircularSettingsOverlay \
