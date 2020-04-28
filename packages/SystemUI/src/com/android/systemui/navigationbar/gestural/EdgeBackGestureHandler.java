4b3318990bb3543b4b148fcf42d28664fbe836c9
DennySPb <dennyspb@gmail.com>
2020-04-28 14:19:10 +0300

    Add switch last app to gesture actions [1/2]
    also move it to ActionUtils
    
    Change-Id: I3690677bce0cdc85951b38790cf9374346b1e6c2
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1130,6 +1130,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             case 13: // Kill app
                 SyberiaUtils.killForegroundApp();
                 break;
+            case 14: // Switch recent app
+                ActionUtils.switchToLastApp(mContext);
+                break;
         }
     }
 
