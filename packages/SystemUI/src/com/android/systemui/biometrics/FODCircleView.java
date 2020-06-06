ca88221fcd268a0417b45896b1fb60727b9636fe
carpe462 <carpe462@gmail.com>
2020-06-06 09:03:36 -0700

    FODCircleView: Clean up redundant updateStyle() and image resource calls
    Change-Id: I877cc3c7a2d69f8de1494c887024961058627c5a

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -258,27 +258,15 @@ public class FODCircleView extends ImageView {
 
         mWindowManager.addView(this, mParams);
 
-        updateStyle();
+
         updatePosition();
+
         hide();
 
         mLockPatternUtils = new LockPatternUtils(mContext);
 
         mUpdateMonitor = KeyguardUpdateMonitor.getInstance(context);
         mUpdateMonitor.registerCallback(mMonitorCallback);
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
-        if (!mIsCircleShowing) {
-            setImageResource(ICON_STYLES[mSelectedIcon]);
-        }
-
-        if (mIsCircleShowing) {
-            setImageResource(PRESSED_STYLES[mPressedIcon]);
-        }
     }
 
     @Override
@@ -370,7 +358,7 @@ public class FODCircleView extends ImageView {
         setDim(true);
 
         dispatchPress();
-        updateStyle();
+
         setImageResource(PRESSED_STYLES[mPressedIcon]);
         invalidate();
     }
