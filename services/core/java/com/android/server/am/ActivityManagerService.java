685cb58c134d466f4a8038fecafd2dba26b2a8d5
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-06-08 21:35:58 +0300

    Make sensor block package list configurable [1/2]
    Based on https://github.com/crdroidandroid/android_frameworks_base/commit/2f0ff544cee1de199856958405e08ad074ee443e
    
    Co-Authored-By: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I3296877e4872437a497ab07db9ac5012d4b7e5ee

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -230,6 +230,8 @@ import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.graphics.Rect;
 import android.hardware.display.DisplayManagerInternal;
+import android.hardware.SensorManager;
+import android.hardware.SystemSensorManager;
 import android.location.LocationManager;
 import android.media.audiofx.AudioEffect;
 import android.net.Proxy;
@@ -1616,6 +1618,7 @@ public class ActivityManagerService extends IActivityManager.Stub
     private CutoutFullscreenController mCutoutFullscreenController;
 
     private GamingModeController mGamingModeController;
+    private SystemSensorManager mSystemSensorManager;
 
     /**
      * Used to notify activity lifecycle events.
@@ -7669,6 +7672,8 @@ public class ActivityManagerService extends IActivityManager.Stub
 
         // Gaming mode provider
         mGamingModeController = new GamingModeController(mContext);
+
+        mSystemSensorManager = new SystemSensorManager(mContext, mHandler.getLooper());
     }
 
     void startPersistentApps(int matchFlags) {
@@ -15181,6 +15186,9 @@ public class ActivityManagerService extends IActivityManager.Stub
                                         mBatteryStatsService.notePackageUninstalled(ssp);
                                         if (mGamingModeController != null) {
                                              mGamingModeController.notePackageUninstalled(ssp);
+                                        }
+                                        if (mSystemSensorManager != null) {
+                                             mSystemSensorManager.notePackageUninstalled(ssp);
                                         }
                                     }
                                 } else {
