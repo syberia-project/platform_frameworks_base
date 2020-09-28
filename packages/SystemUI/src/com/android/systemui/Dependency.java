566537cc1cdb18a1ab92e00b530c59549c17e7e8
maxwen <max.weninger@gmail.com>
2020-09-28 15:40:30 +0200

    SystemUI: add SyberiaSettingsService
    Change-Id: I7909aab11b352fb058bf122102d85b095418fbb9
    Signed-off-by: blinoff82 <blinov.in@gmail.com>

--- packages/SystemUI/src/com/android/systemui/Dependency.java
+++ packages/SystemUI/src/com/android/systemui/Dependency.java
@@ -48,6 +48,7 @@ import com.android.systemui.keyguard.ScreenLifecycle;
 import com.android.systemui.keyguard.WakefulnessLifecycle;
 import com.android.systemui.media.dialog.MediaOutputDialogFactory;
 import com.android.systemui.model.SysUiState;
+import com.android.systemui.syberia.SyberiaSettingsService;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.DarkIconDispatcher;
 import com.android.systemui.plugins.PluginDependencyProvider;
@@ -328,6 +329,7 @@ public class Dependency {
     @Inject Lazy<Divider> mDivider;
     @Inject Lazy<TaskHelper> mTaskHelper;
     @Inject Lazy<MediaOutputDialogFactory> mMediaOutputDialogFactory;
+    @Inject Lazy<SyberiaSettingsService> mSyberiaSettingsService;
 
     @Inject
     public Dependency() {
@@ -526,6 +528,7 @@ public class Dependency {
 
         mProviders.put(RecordingController.class, mRecordingController::get);
         mProviders.put(Divider.class, mDivider::get);
+        mProviders.put(SyberiaSettingsService.class, mSyberiaSettingsService::get);
 
         mProviders.put(MediaOutputDialogFactory.class, mMediaOutputDialogFactory::get);
 
