10fa952eeac6623a478f1686730d4b2afd02b9fa
Danny Baumann <dannybaumann@web.de>
2020-11-05 19:13:14 +0000

    Add back increasing ring feature [1/3]
    Change-Id: I2cd34e08badb343fa418ed07e6733e3fbe7d7d5d
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: blinoff82 <blinov.in@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6018,6 +6018,24 @@ public final class Settings {
         public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
 
         /**
+         * Whether the phone ringtone should be played in an increasing manner
+         * @hide
+         */
+        public static final String INCREASING_RING = "increasing_ring";
+
+        /**
+         * Start volume fraction for increasing ring volume
+         * @hide
+         */
+        public static final String INCREASING_RING_START_VOLUME = "increasing_ring_start_vol";
+
+        /**
+         * Ramp up time (seconds) for increasing ring
+         * @hide
+         */
+        public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
+
+        /**
          * These are all public system settings
          *
          * @hide
