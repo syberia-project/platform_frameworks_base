b54499f8fdcd3f139658491f76021d8d0f8acf99
SuperDroidBond <SuperDroidBond@users.noreply.github.com>
2020-11-11 20:36:55 +0000

    SystemUI: KG Charging Animation: Add more animations [1/3]
    Special Thanks to Rodolphe06, Flowdor Theme Dev for the json files
    
    neobuddy89: Smarter coding. :)
    
    spezi77: Adapted for SettingsObserver
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Sagarrokade006 <Sagarrokade006@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I36ec8a4584378858f6d13cd28ef6a94dc0b17467

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -606,7 +606,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     private final NotificationRemoteInputManager mRemoteInputManager;
     private boolean mWallpaperSupported;
 	
-    private boolean mChargingAnimation;
+    private int mChargingAnimation;
 
     private final BroadcastReceiver mWallpaperChangedReceiver = new BroadcastReceiver() {
         @Override
@@ -789,7 +789,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                     Settings.System.SWITCH_STYLE),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.LOCKSCREEN_CHARGING_ANIMATION),
+                    Settings.System.LOCKSCREEN_CHARGING_ANIMATION_STYLE),
                     false, this, UserHandle.USER_ALL);
         }
 
@@ -809,7 +809,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 stockSwitchStyle();
                 updateSwitchStyle();
             } else if (uri.equals(Settings.System.getUriFor(
-                    Settings.System.LOCKSCREEN_CHARGING_ANIMATION))) {
+                    Settings.System.LOCKSCREEN_CHARGING_ANIMATION_STYLE))) {
                 updateChargingAnimation();
             }
             update();
@@ -2398,7 +2398,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 	
     private void updateChargingAnimation() {
         mChargingAnimation = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.LOCKSCREEN_CHARGING_ANIMATION, 1, UserHandle.USER_CURRENT) == 1;
+                Settings.System.LOCKSCREEN_CHARGING_ANIMATION_STYLE, 1, UserHandle.USER_CURRENT);
         if (mKeyguardIndicationController != null) {
             mKeyguardIndicationController.updateChargingIndication(mChargingAnimation);
         }
