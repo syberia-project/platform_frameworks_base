d6f264a2da64f24efc14930782cdc8e933e9fb74
HrX03 <dn.bianco03@gmail.com>
2021-02-15 14:43:39 +0100

    fwb: Implement VolumePluginManager
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I676b6552a100dbd4adde7d305e77abfe3341de36

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5943,6 +5943,12 @@ public final class Settings {
         public static final String UI_BACKGROUND_BLUR = "ui_background_blur";
 
         /**
+         * Holds current enabled volume panel implementation
+         * @hide
+         */
+        public static final String SYSTEMUI_PLUGIN_VOLUME = "systemui_plugin_volume";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
