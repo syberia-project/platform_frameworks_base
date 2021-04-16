04ce6821f7311363c3aa8ebafbdd930fe86e41f5
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-04-16 13:51:49 +0530

    SystemUI: Fix multiple issues with typographic clock
    - do not add add burnin offset unless burnin protection is enabled
    - left align notifications in aod
    - left align owner info on lockscreen
    - improve spacing
    - misc code cleanups
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I7d5c091accf47bfb972e90667719a4ea492255e0

--- packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardClockSwitch.java
@@ -232,7 +232,7 @@ public class KeyguardClockSwitch extends RelativeLayout {
             mClockPlugin.onDestroyView();
             mClockPlugin = null;
         }
-        adjustStatusAreaPadding(plugin);
+        mKeyguardStatusArea.setRowGravity(isTypeClock(plugin) ? Gravity.START : Gravity.CENTER);
         if (plugin == null) {
             if (mShowingHeader) {
                 mClockView.setVisibility(View.GONE);
@@ -444,12 +444,12 @@ public class KeyguardClockSwitch extends RelativeLayout {
         }
     }
 
-    private void adjustStatusAreaPadding(ClockPlugin plugin) {
-        final boolean mIsTypeClock = plugin != null && plugin.getName().equals("type");
-        mKeyguardStatusArea.setRowGravity(mIsTypeClock ? Gravity.LEFT : Gravity.CENTER);
-        mKeyguardStatusArea.setRowPadding(mIsTypeClock ? mContext.getResources()
-                .getDimensionPixelSize(R.dimen.keyguard_status_area_typeclock_padding) : 0, 0, 0,
-                0);
+    private boolean isTypeClock(ClockPlugin plugin) {
+        return plugin != null && plugin.getName().equals("type");
+    }
+
+    public boolean isTypeClock() {
+        return isTypeClock(mClockPlugin);
     }
 
     /**
