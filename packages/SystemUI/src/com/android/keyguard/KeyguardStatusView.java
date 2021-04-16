ac30f84ae6a3c59fb0e989cb252096eb727bc810
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-04-16 13:51:49 +0530

    SystemUI: Fix multiple issues with typographic clock
    - do not add add burnin offset unless burnin protection is enabled
    - left align notifications in aod
    - left align owner info on lockscreen
    - improve spacing
    - misc code cleanups
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I7d5c091accf47bfb972e90667719a4ea492255e0

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -33,6 +33,7 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.util.Slog;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
@@ -45,6 +46,7 @@ import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.Interpolators;
 import com.android.systemui.omni.CurrentWeatherView;
+import com.android.systemui.statusbar.phone.NotificationIconContainer;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 
 import java.io.FileDescriptor;
@@ -66,7 +68,7 @@ public class KeyguardStatusView extends GridLayout implements
     private KeyguardClockSwitch mClockView;
     private TextView mOwnerInfo;
     private KeyguardSliceView mKeyguardSlice;
-    private View mNotificationIcons;
+    private NotificationIconContainer mNotificationIcons;
     private Runnable mPendingMarqueeStart;
     private Handler mHandler;
 
@@ -156,6 +158,10 @@ public class KeyguardStatusView extends GridLayout implements
         return mClockView.hasCustomClock();
     }
 
+    public boolean isTypeClock() {
+        return mClockView.isTypeClock();
+    }
+
     /**
      * Set whether or not the lock screen is showing notifications.
      */
@@ -225,6 +231,9 @@ public class KeyguardStatusView extends GridLayout implements
      * Moves clock, adjusting margins when slice content changes.
      */
     private void onSliceContentChanged() {
+        if (mNotificationIcons != null)
+            mNotificationIcons.setCenter(isTypeClock() ? false : true);
+
         final boolean hasHeader = mKeyguardSlice.hasHeader();
         mClockView.setKeyguardShowingHeader(hasHeader);
         if (mShowingHeader == hasHeader) {
@@ -236,8 +245,7 @@ public class KeyguardStatusView extends GridLayout implements
             MarginLayoutParams params = (MarginLayoutParams) mNotificationIcons.getLayoutParams();
             params.setMargins(params.leftMargin,
                     hasHeader ? mIconTopMarginWithHeader : mIconTopMargin,
-                    params.rightMargin,
-                    params.bottomMargin);
+                    params.rightMargin, params.bottomMargin);
             mNotificationIcons.setLayoutParams(params);
         }
     }
@@ -327,6 +335,7 @@ public class KeyguardStatusView extends GridLayout implements
             }
         }
         mOwnerInfo.setText(info);
+        mOwnerInfo.setGravity(isTypeClock() ? Gravity.START : Gravity.CENTER_HORIZONTAL);
         updateDark();
     }
 
