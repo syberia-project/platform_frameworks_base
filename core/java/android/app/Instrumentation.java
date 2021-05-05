4f4c53c9b6c3fb14f71341736934fd2ebf01eedf
Danny Lin <danny@kdrag0n.dev>
2021-05-05 02:01:39 -0700

    PixelPropsUtils: Make build property spoofing more reliable
    Determining the package name from Zygote's post-fork process name is
    unreliable for several reasons:
      - If USAP is enabled, Zygote will fork with the USAP process name, and
        spoofing will never trigger when the forked process specializes into
        a specific app.
      - With the native fork loop backported from AOSP master,
        Zygote.setAppProcessName() is no longer used for most apps because
        the forking is handled by native code.
    
    Instrumentation#newApplication() is responsible for creating the
    Application singleton during each app's initialization, so hook it
    instead for a more reliable and robust trigger.
    
    Change-Id: Ic762d8853967bf78db614efaf46166e527cb397a
    Signed-off-by: Kunmun <kunmun.devroms@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -66,6 +66,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
+import com.android.internal.util.custom.PixelPropsUtils;
+
 /**
  * Base class for implementing application instrumentation code.  When running
  * with instrumentation turned on, this class will be instantiated for you
@@ -1179,6 +1181,8 @@ public class Instrumentation {
                 .instantiateApplication(cl, className);
         app.attach(context);
         AttestationHooks.initApplicationBeforeOnCreate(app);
+        String packageName = app.getPackageName();
+        PixelPropsUtils.setProps(packageName);
         return app;
     }
     
@@ -1197,6 +1201,8 @@ public class Instrumentation {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
         AttestationHooks.initApplicationBeforeOnCreate(app);
+        String packageName = app.getPackageName();
+        PixelPropsUtils.setProps(packageName);
         return app;
     }
 
