b2e3cccfa078eb756cd1c9e10eab2d0d4a83d73a
DennySPb <dennyspb@gmail.com>
2021-05-12 10:03:07 +0300

    Revert "SystemUI: Fix issues with VoWiFi icon"
    This reverts commit 6de95fe3afd19b19cb056b06471a441caf3fb784.
    
    Change-Id: Ib512d890c1eb1f5019a1ff86fc9099b96db0570e

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MobileSignalController.java
@@ -116,7 +116,6 @@ public class MobileSignalController extends SignalController<
     private int mVoWiFiIcon;
     // VoWiFi Icon Style
     private int mVoWiFistyle;
-    private boolean mIsVowifiAvailable;
 
     // TODO: Reduce number of vars passed in, if we have the NetworkController, probably don't
     // need listener lists anymore.
@@ -557,13 +556,11 @@ public class MobileSignalController extends SignalController<
         mCurrentState.voiceCapable = tm.isVolteAvailable();
         mCurrentState.videoCapable = tm.isVideoTelephonyAvailable();
         mCurrentState.imsRegistered = mPhone.isImsRegistered(mSubscriptionInfo.getSubscriptionId());
-        mIsVowifiAvailable = tm.isWifiCallingAvailable();
         if (DEBUG) {
             Log.d(mTag, "queryImsState tm=" + tm + " phone=" + mPhone
                     + " voiceCapable=" + mCurrentState.voiceCapable
                     + " videoCapable=" + mCurrentState.videoCapable
-                    + " imsRegistered=" + mCurrentState.imsRegistered
-                    + " mIsVowifiAvailable=" + mIsVowifiAvailable);
+                    + " imsResitered=" + mCurrentState.imsRegistered);
         }
         notifyListenersIfNecessary();
     }
@@ -862,8 +859,7 @@ public class MobileSignalController extends SignalController<
 
     private boolean isVowifiAvailable() {
         return mCurrentState.voiceCapable &&  mCurrentState.imsRegistered
-                && (getDataNetworkType() == TelephonyManager.NETWORK_TYPE_IWLAN
-                || mIsVowifiAvailable);
+                && getDataNetworkType() == TelephonyManager.NETWORK_TYPE_IWLAN;
     }
 
     private MobileIconGroup getVowifiIconGroup() {
