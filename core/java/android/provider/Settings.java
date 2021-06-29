11bffa7468fbbd0a954af9bd620dc93b111ecdfe
spkal01 <kalligeross@gmail.com>
2021-06-29 23:47:45 +0530

    Introduce Brightness Slider Styles [1/2]
    Signed-off-by: spkal01 <kalligeross@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Change-Id: I166416459b6d805a5b7c7a4e2de1e5b608b40016

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5989,6 +5989,11 @@ public final class Settings {
         public static final String LOCKSCREEN_ENABLE_QS = "lockscreen_enable_qs";
 
         /**
+         * @hide
+         */
+        public static final String SLIDER_STYLE = "slider_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
