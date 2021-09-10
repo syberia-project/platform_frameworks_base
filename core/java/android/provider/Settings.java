4216e6b633cb43710dd18b220b713b1aee4a84dc
DennySPb <dennyspb@gmail.com>
2021-09-10 13:59:31 +0300

    GamingMode: Option for disable menu overlay [1/3]
        * I found a game (Oddmar) that does not work with the touchscreen
          if the overlay is enabled. There may be others.
    
    Change-Id: Ic3de104aaf85be166218c98868135155e234124a
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6056,6 +6056,11 @@ public final class Settings {
         public static final String GAMING_MODE_USE_GAME_DRIVER = "gaming_mode_use_game_driver";
 
         /**
+          * @hide
+          */
+        public static final String GAMING_MODE_USE_OVERLAY_MENU = "gaming_mode_use_overlay_menu";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
