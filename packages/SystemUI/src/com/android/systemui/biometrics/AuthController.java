24bb80d73a9c04ec1437d20b0f1ca269fe483428
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-10-16 21:53:59 +0530

    SystemUI: AuthController: Fix ripple animation for side fp devices
     * start animation from right side of display and not middle,
       as thats where the powerbutton/fingerprint sensor is located
       on these devices
    
    Squashed:
    
    SystemUI: AuthController: Fix NPE due to isPowerbuttonFps()
    
     * in some cases mFingerprintManager is null and crashes systemui
     * add null check for it like in other instances
    
    Fixes: 7551de8ea8fb0c0407f5ff53563b86b606e23f0b ("SystemUI: AuthController: Fix ripple animation for side fp devices")
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Ic67f31ab2cf44c85e52bfe800d850b4e24a2a337
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/biometrics/AuthController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/AuthController.java
@@ -479,7 +479,11 @@ public class AuthController extends SystemUI implements CommandQueue.Callbacks,
                     (float) faceAuthLocation[1]);
         }
 
-        mFingerprintLocation = new PointF(DisplayUtils.getWidth(mContext) / 2,
+        boolean isPowerbuttonFps =
+                mFingerprintManager != null && mFingerprintManager.isPowerbuttonFps();
+
+        mFingerprintLocation = new PointF(
+                DisplayUtils.getWidth(mContext) / (isPowerbuttonFps ? 1 : 2),
                 mContext.getResources().getDimensionPixelSize(
                 com.android.systemui.R.dimen.physical_fingerprint_sensor_center_screen_location_y));
 
