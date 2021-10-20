ac252583ccd001fd8f4f4072527b4f65dd26e259
DennySPb <dennyspb@gmail.com>
2021-10-20 16:00:45 +0300

    LiveDisplay: Don't break lint
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I6a8abb94682e883b0644996f0f09808bd594aa65
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6046,18 +6046,21 @@ public final class Settings {
         public static final String DISPLAY_TEMPERATURE_DAY = "display_temperature_day";
 
         /**
+         * @hide
          * Color temperature of the display at night
          * @hide
          */
         public static final String DISPLAY_TEMPERATURE_NIGHT = "display_temperature_night";
 
         /**
+         * @hide
          * Display color temperature adjustment mode, one of DAY (default), NIGHT, or AUTO.
          * @hide
          */
         public static final String DISPLAY_TEMPERATURE_MODE = "display_temperature_mode";
 
         /**
+         * @hide
          * Automatic outdoor mode
          * 0 = 0ff, 1 = on
          * @hide
@@ -6065,12 +6068,14 @@ public final class Settings {
         public static final String DISPLAY_AUTO_OUTDOOR_MODE = "display_auto_outdoor_mode";
 
         /**
+         * @hide
          * Anti flicker
          * 0 = 0ff, 1 = on
          */
         public static final String DISPLAY_ANTI_FLICKER = "display_anti_flicker";
 
         /**
+         * @hide
          * Reader mode
          * 0 = 0ff, 1 = on
          * @hide
@@ -6078,6 +6083,7 @@ public final class Settings {
         public static final String DISPLAY_READING_MODE = "display_reading_mode";
 
         /**
+         * @hide
          * Use display power saving features such as CABC or CABL
          * 0 = 0ff, 1 = on
          * @hide
@@ -6085,6 +6091,7 @@ public final class Settings {
         public static final String DISPLAY_CABC = "display_low_power";
 
         /**
+         * @hide
          * Use color enhancement feature of display
          * 0 = 0ff, 1 = on
          * @hide
@@ -6092,6 +6099,7 @@ public final class Settings {
         public static final String DISPLAY_COLOR_ENHANCE = "display_color_enhance";
 
         /**
+         * @hide
          * Use auto contrast optimization feature of display
          * 0 = 0ff, 1 = on
          * @hide
@@ -6099,21 +6107,22 @@ public final class Settings {
         public static final String DISPLAY_AUTO_CONTRAST = "display_auto_contrast";
 
         /**
+         * @hide
          * Manual display color adjustments (RGB values as floats, separated by spaces)
          * @hide
          */
         public static final String DISPLAY_COLOR_ADJUSTMENT = "display_color_adjustment";
 
         /**
-         * The current custom picture adjustment values as a delimited string
          * @hide
+         * The current custom picture adjustment values as a delimited string
          */
         public static final String DISPLAY_PICTURE_ADJUSTMENT =
                 "display_picture_adjustment";
 
         /**
-         * Did we tell about how they can stop breaking their eyes?
          * @hide
+         * Did we tell about how they can stop breaking their eyes?
          */
         public static final String LIVE_DISPLAY_HINTED = "live_display_hinted";
 
