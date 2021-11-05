6331fcd46381dd489d0636dbea52c5bcf7f98023
tysonwei <tysonwei@realtek.com>
2021-11-05 18:02:28 +0800

    Fix potential race condition problem
    If we call forceSuspend to enter STR and then resume from it,
    we should make sure that mForceSuspendActive is set to false
    to wake up device correctly.
    
    Bug: 205257123
    Test: 1. forceSuspend
          2. wait until kernel enter suspend mode
          3. press power button to wakeUp
    
    Change-Id: I85c9fd2aaed602cc927b11d256b88965c95ae8d5

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1852,7 +1852,7 @@ public final class PowerManagerService extends SystemService
                     + ", groupId=" + groupId + ", uid=" + uid);
         }
 
-        if (eventTime < mLastSleepTime || mForceSuspendActive || !mSystemReady) {
+        if (eventTime < mLastSleepTime || !mSystemReady) {
             return false;
         }
 
@@ -4060,39 +4060,35 @@ public final class PowerManagerService extends SystemService
     }
 
     private boolean forceSuspendInternal(int uid) {
-        try {
-            synchronized (mLock) {
-                mForceSuspendActive = true;
-                // Place the system in an non-interactive state
-                boolean updatePowerState = false;
-                for (int id : mDisplayGroupPowerStateMapper.getDisplayGroupIdsLocked()) {
-                    updatePowerState |= sleepDisplayGroupNoUpdateLocked(id, mClock.uptimeMillis(),
-                            PowerManager.GO_TO_SLEEP_REASON_FORCE_SUSPEND,
-                            PowerManager.GO_TO_SLEEP_FLAG_NO_DOZE, uid);
-                }
-                if (updatePowerState) {
-                    updatePowerStateLocked();
-                }
-
-                // Disable all the partial wake locks as well
-                updateWakeLockDisabledStatesLocked();
+        synchronized (mLock) {
+            mForceSuspendActive = true;
+            // Place the system in an non-interactive state
+            boolean updatePowerState = false;
+            for (int id : mDisplayGroupPowerStateMapper.getDisplayGroupIdsLocked()) {
+                updatePowerState |= sleepDisplayGroupNoUpdateLocked(id, mClock.uptimeMillis(),
+                        PowerManager.GO_TO_SLEEP_REASON_FORCE_SUSPEND,
+                        PowerManager.GO_TO_SLEEP_FLAG_NO_DOZE, uid);
             }
+            if (updatePowerState) {
+                updatePowerStateLocked();
+            }
+
+            // Disable all the partial wake locks as well
+            updateWakeLockDisabledStatesLocked();
 
             Slog.i(TAG, "Force-Suspending (uid " + uid + ")...");
             boolean success = mNativeWrapper.nativeForceSuspend();
             if (!success) {
                 Slog.i(TAG, "Force-Suspending failed in native.");
             }
+
+            mForceSuspendActive = false;
+            // Re-enable wake locks once again.
+            updateWakeLockDisabledStatesLocked();
+
             return success;
-        } finally {
-            synchronized (mLock) {
-                mForceSuspendActive = false;
-                // Re-enable wake locks once again.
-                updateWakeLockDisabledStatesLocked();
-            }
         }
     }
-
     /**
      * Low-level function turn the device off immediately, without trying
      * to be clean.  Most people should use {@link ShutdownThread} for a clean shutdown.
