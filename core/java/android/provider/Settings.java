2116b24d6b93cf7839ce37310e09b2afdd719a23
spkal01 <spkal01@users.noreply.github.com>
2021-12-09 18:32:21 +0000

    Make colored Statusbar Icons optional [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: blinoff82 <blinov.in@gmail.com>
    Change-Id: I1e698f44ba9905eb912aa0b34d93e9030850eb21
    Signed-off-by: blinoff82 <blinov.in@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6216,6 +6216,12 @@ public final class Settings {
         public static final String UDFPS_ICON = "udfps_icon";
 
         /**
+         * Whether StatusBar icons should use app icon
+         * @hide
+         */
+        public static final String STATUSBAR_COLORED_ICONS = "statusbar_colored_icons";
+
+        /**
          * These are all public system settings
          *
          * @hide
