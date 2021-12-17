2e0604f7cda642e9983b22aa5dc18b36c016b6b3
jhenrique09 <jhsv09@gmail.com>
2021-12-17 10:40:44 +0900

    SystemUI: Port elmyra (squeeze) from SQ1D
     * Required by google assistmanager
    
    Co-authored-by: Anay Wadhera <anay1018@gmail.com>
    [ghostrider-reborn: adapt to aospa]
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Ib6051018627088c318a40816064ddf95408439e0
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/syberia/android/systemui/dagger/SyberiaSystemUIModule.java
+++ packages/SystemUI/src/com/syberia/android/systemui/dagger/SyberiaSystemUIModule.java
@@ -134,6 +134,7 @@ import com.google.android.systemui.assist.uihints.input.NgaInputHandler;
 import com.google.android.systemui.assist.uihints.input.TouchActionRegion;
 import com.google.android.systemui.assist.uihints.input.TouchInsideRegion;
 import com.google.android.systemui.columbus.ColumbusServiceWrapper;
+import com.google.android.systemui.elmyra.ServiceConfigurationGoogle;
 import com.google.android.systemui.smartspace.BcSmartspaceDataProvider;
 import com.google.android.systemui.smartspace.KeyguardMediaViewController;
 import com.google.android.systemui.smartspace.KeyguardZenAlarmViewController;
@@ -290,8 +291,8 @@ public abstract class SyberiaSystemUIModule {
 
     @Provides
     @SysUISingleton
-    static SyberiaServices provideSyberiaServices(Context context, UiEventLogger uiEventLogger, Lazy<ColumbusServiceWrapper> lazyB) {
-        return new SyberiaServices(context, uiEventLogger, lazyB);
+    static SyberiaServices provideSyberiaServices(Context context, UiEventLogger uiEventLogger, Lazy<ServiceConfigurationGoogle> lazy, Lazy<ColumbusServiceWrapper> lazyB) {
+        return new SyberiaServices(context, uiEventLogger, lazy, lazyB);
     }
 
     // Google
