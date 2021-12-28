9d1243101b8e6b84e2eacdbe1ff7dc74bf75770e
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 05:49:21 +0530

    SystemUI: Initial Syberia SystemUI stub
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I67f36f20cc01dfc82572f2241287c3e739592306
    Signed-off-by: DennySPb <dennyspb@gmail.com>

---
+++ packages/SystemUI/src/com/syberia/android/systemui/dagger/SyberiaSysUISubcomponentModule.java
@@ -0,0 +1,10 @@
+package com.syberia.android.systemui.dagger;
+
+import dagger.Module;
+
+/**
+ * Dagger module for including the WMComponent.
+ */
+@Module(subcomponents = {SyberiaSysUIComponent.class})
+public abstract class SyberiaSysUISubcomponentModule {
+}
