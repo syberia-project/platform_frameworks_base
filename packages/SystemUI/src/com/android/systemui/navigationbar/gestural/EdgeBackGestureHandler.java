4bef8ffb0175bc3dad4422c08d8d133f32dc7c4f
DennySPb <dennyspb@gmail.com>
2022-02-18 11:14:12 +0300

    EdgeBackGesture: Remove assistant action
        * Have NPE with columbus
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Id29597df37d75d830d7b99d2cacb817830ba9e77
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -66,9 +66,7 @@ import com.android.internal.util.hwkeys.ActionUtils;
 import com.android.internal.util.syberia.SyberiaUtils;
 import com.android.internal.config.sysui.SystemUiDeviceConfigFlags;
 import com.android.internal.policy.GestureNavigationSettingsObserver;
-import com.android.systemui.Dependency;
 import com.android.systemui.R;
-import com.android.systemui.assist.AssistManager;
 import com.android.systemui.SystemUIFactory;
 import com.android.systemui.broadcast.BroadcastDispatcher;
 import com.android.systemui.dagger.qualifiers.Main;
@@ -235,7 +233,6 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
     private boolean mInRejectedExclusion = false;
     private boolean mIsOnLeftEdge;
 
-    private AssistManager mAssistManager;
     private int mTimeout = 2000; //ms
     private int mLeftLongSwipeAction;
     private int mRightLongSwipeAction;
@@ -376,7 +373,6 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 mContext.getMainThreadHandler(), mContext, this::onNavigationSettingsChanged);
 
         mHandler = new Handler();
-        mAssistManager = Dependency.get(AssistManager.class);
         updateCurrentUserResources();
     }
 
@@ -1006,8 +1002,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
             case 0: // No action
             default:
                 break;
-            case 1: // Assistant
-                mAssistManager.startAssist(new Bundle() /* args */);
+            case 1:
                 break;
             case 2: // Voice search
                 ActionUtils.launchVoiceSearch(mContext);
