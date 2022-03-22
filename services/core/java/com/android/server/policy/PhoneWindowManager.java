1c4704cacb1a9f87f1a7e2c090009cf7b8ea8c2e
DennySPb <dennyspb@gmail.com>
2022-03-22 11:24:55 +0300

    Revert "Remove keyguard candidate."
    This reverts commit a9573379e273b36e3cb89da822d3e4e987c457fd.
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -421,6 +421,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private AccessibilityShortcutController mAccessibilityShortcutController;
 
     boolean mSafeMode;
+    private WindowState mKeyguardCandidate = null;
 
     // Whether to allow dock apps with METADATA_DOCK_HOME to temporarily take over the Home key.
     // This is for car dock and this is updated from resource.
@@ -3511,6 +3512,14 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     @Override
     public void setNavBarVirtualKeyHapticFeedbackEnabledLw(boolean enabled) {
         mNavBarVirtualKeyHapticFeedbackEnabled = enabled;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void setKeyguardCandidateLw(WindowState win) {
+        mKeyguardCandidate = win;
+        setKeyguardOccludedLw(isKeyguardOccluded(), true /* force */,
+                false /* keyguardOccludingStarted */);
     }
 
     /**
