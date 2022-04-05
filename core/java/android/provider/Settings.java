784cb4edb4e7909261b8827b5d9e8e3082089936
jhonboy121 <alfredmathew05@gmail.com>
2022-04-05 12:14:45 +0530

    base: allow hiding apps from launcher [1/2]
    Change-Id: I118a6ae1c29af04e4b8e000d00538cef5361b02a
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11960,6 +11960,13 @@ public final class Settings {
          */
         public static final String QS_BRIGHTNESS_SLIDER_POSITION = "qs_brightness_slider_position";
 
+
+        /**
+         * List of ; separated apps to be hidden from launcher.
+         * @hide
+         */
+        public static final String LAUNCHER_HIDDEN_APPS = "launcher_hidden_apps";
+
         /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
