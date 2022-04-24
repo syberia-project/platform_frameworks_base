be598a797a4eebb7d0d3e148e9f89f2f99f73dde
Genkzsz11 <genkzsz11@gmail.com>
2022-04-24 19:17:12 +0700

    SystemUI: Hide oos clear all if reticker enable
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Iec8631dc136a017707f028171fcf862587ff1989

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -5076,6 +5076,7 @@ public class NotificationPanelViewController extends PanelViewController {
         boolean debug = true;
         if (visibility && getExpandedFraction() != 1) {
             mNotificationStackScroller.setVisibility(GONE);
+            mStatusBar.updateDismissAllVisibility(false);
             ExpandableNotificationRow row = mHeadsUpManager.getTopEntry().getRow();
             String pkgname = row.getEntry().getSbn().getPackageName();
             Drawable icon = null;
