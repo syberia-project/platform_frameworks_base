2a721377a84d81501e6b92b71beb965012c5d6f2
LuK1337 <priv.luk@gmail.com>
2022-05-06 21:26:57 +0900

    core: Add ENABLE_TASKBAR setting
    Change-Id: Ice87961f32432cd9fab750ea6aba5bdab9f16a83

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6253,6 +6253,12 @@ public final class Settings {
 
 
         /**
+         * Whether to enable taskbar.
+         * @hide
+         */
+        public static final String ENABLE_TASKBAR = "enable_taskbar";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
