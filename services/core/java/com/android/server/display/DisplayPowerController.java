3bfe7b72bbfce9457783ab17d9db0242eeea9e94
DennySPb <dennyspb@gmail.com>
2022-06-02 12:42:59 +0300

    Revert "DisplayPowerController: Don't apply brightness adjustment if NaN"
    This reverts commit 1dbc353662a74f37f66952c78182e352a9a397d9.
    
    Change-Id: I721a61fd7eb3bf1e49edc339e34961fce4dda5a7

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -2124,8 +2124,6 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
     }
 
     private void putAutoBrightnessAdjustmentSetting(float adjustment) {
-        if (Float.isNaN(adjustment))
-            return;
         if (mDisplayId == Display.DEFAULT_DISPLAY) {
             mAutoBrightnessAdjustment = adjustment;
             Settings.System.putFloatForUser(mContext.getContentResolver(),
