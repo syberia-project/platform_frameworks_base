90446ef1b78b94b865d17cb1d4e3c29ab115c352
ke feng <fengke1@oppo.com>
2022-06-02 07:48:41 +0000

    service/provider adj should not delivery to client when cycleReEval computeOomAdjLSP in Cycle strategy.
    1. computeOomAdjLSP -> Cycle strategy -> computeOomAdjLSP with cachedAdj = state.getCurRawAdj() -> computeClients computeOomAdjLSP deliver the service or provider cachedAdj to client
    2. cachedAdj will let client have service adj which is not right
    
    =>
    question:
    updateOomAdjInnerLSP function
    1) if D->A->B->C->A
    A process is client for D process
    B process is client for A process
    C process is client for B process
    A process is client for C process
    
    2) first computeOomAdjLSP all ABCD will be setContainsCycle(true)
    
    3) second Cycle strategy:
    => if mLruProcesses is order by C, B, A, D
    => if C getCurRawAdj() is 0
    then:
    => C->A->B->C will let ABC all adj = 0
    C cachedAdj = getCurRawAdj() = 0, which will deliver to all computeClients computeOomAdjLSP (B, C adj is raise by cachedAdj(C's getCurRawAdj()))
    => D->A (D will be raise adj by A, but A adj may be not right here)
    
    4) Fix:
    => C->A->B->C will let ABC all adj = 0
    patch will stop C's getCurRawAdj() deliver to A, A use A's getCurRawAdj(which is A process not C process)
    
    Change-Id: I4aa7fed1301a517869b8d14ed5d32adeab42aeae
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- services/core/java/com/android/server/am/OomAdjuster.java
+++ services/core/java/com/android/server/am/OomAdjuster.java
@@ -1946,7 +1946,7 @@ public class OomAdjuster {
                     ProcessRecord client = cr.binding.client;
                     final ProcessStateRecord cstate = client.mState;
                     if (computeClients) {
-                        computeOomAdjLSP(client, cachedAdj, topApp, doingAll, now,
+                        computeOomAdjLSP(client, cycleReEval ? cstate.getCurRawAdj() : cachedAdj, topApp, doingAll, now,
                                 cycleReEval, true);
                     } else {
                         cstate.setCurRawAdj(cstate.getCurAdj());
@@ -2262,7 +2262,7 @@ public class OomAdjuster {
                     continue;
                 }
                 if (computeClients) {
-                    computeOomAdjLSP(client, cachedAdj, topApp, doingAll, now, cycleReEval, true);
+                    computeOomAdjLSP(client, cycleReEval ? cstate.getCurRawAdj() : cachedAdj, topApp, doingAll, now, cycleReEval, true);
                 } else {
                     cstate.setCurRawAdj(cstate.getCurAdj());
                     cstate.setCurRawProcState(cstate.getCurProcState());
