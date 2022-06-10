4ff106a7ba1c589f1a29315affab23cbe13de4b4
anupam41 <anupamsamanta41@gmail.com>
2022-06-10 16:09:42 +0530

    Overlay: Remove Settings icon Overlay
    Signed-off-by: anupam41 <anupamsamanta41@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I2a0b1dd7a48a1b121b2919bd404fd67b1704a02a

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -36,37 +36,30 @@ LOCAL_REQUIRED_MODULES := \
 	FontNotoSerifSourceOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularLauncherOverlay \
-	IconPackCircularSettingsOverlay \
 	IconPackCircularSystemUIOverlay \
 	IconPackCircularThemePickerOverlay \
         IconPackPUIAndroidOverlay \
         IconPackPUILauncherOverlay \
-        IconPackPUISettingsOverlay \
         IconPackPUISystemUIOverlay \
         IconPackPUIThemePickerOverlay \
 	IconPackVictorAndroidOverlay \
 	IconPackVictorLauncherOverlay \
-	IconPackVictorSettingsOverlay \
 	IconPackVictorSystemUIOverlay \
 	IconPackVictorThemePickerOverlay \
 	IconPackSamAndroidOverlay \
 	IconPackSamLauncherOverlay \
-	IconPackSamSettingsOverlay \
 	IconPackSamSystemUIOverlay \
 	IconPackSamThemePickerOverlay \
 	IconPackKaiAndroidOverlay \
 	IconPackKaiLauncherOverlay \
-	IconPackKaiSettingsOverlay \
 	IconPackKaiSystemUIOverlay \
 	IconPackKaiThemePickerOverlay \
 	IconPackFilledAndroidOverlay \
 	IconPackFilledLauncherOverlay \
-	IconPackFilledSettingsOverlay \
 	IconPackFilledSystemUIOverlay \
 	IconPackFilledThemePickerOverlay \
 	IconPackRoundedAndroidOverlay \
 	IconPackRoundedLauncherOverlay \
-	IconPackRoundedSettingsOverlay \
 	IconPackRoundedSystemUIOverlay \
 	IconPackRoundedThemePickerOverlay \
 	FontOneplusSlateSource \
@@ -95,11 +88,9 @@ LOCAL_REQUIRED_MODULES := \
 	FontMontserratSourceOverlay \
         IconPackOOSAndroidOverlay \
         IconPackOOSLauncherOverlay \
-        IconPackOOSSettingsOverlay \
         IconPackOOSSystemUIOverlay \
         IconPackOOSThemePickerOverlay \
         IconPackOutlineAndroidOverlay \
-        IconPackOutlineSettingsOverlay \
         IconPackOutlineSystemUIOverlay \
         IconPackAcherusAndroidOverlay \
         IconPackAcherusSystemUIOverlay \
@@ -119,7 +110,6 @@ LOCAL_REQUIRED_MODULES := \
         IconShapeTeardropOverlay \
         IconShapeVesselOverlay \
         IconPackDoraAndroidOverlay \
-        IconPackDoraSettingsOverlay \
         IconPackDoraSystemUIOverlay \
 	NavigationBarMode3ButtonOverlay \
 	NavigationBarMode2ButtonOverlay \
