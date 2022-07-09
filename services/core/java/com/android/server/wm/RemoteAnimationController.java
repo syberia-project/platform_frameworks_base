ddfa5ecf238183a95b706e1f2e65837dd68e0966
xi yu <yuxi1@xiaomi.com>
2022-07-09 22:11:09 +0800

    Fix IndexOutOfBoundsException in onAnimationFinished of RemoteAnimationController
    Sometimes mPendingAnimations.remove will be called more than once in one cycle (line 255 - 270) of onAnimationFinished, e.g., mPendingAnimations.remove being called again in adapters.mAdapter.mCapturedFinishCallback.onAnimationFinished, resulting in IndexOutOfBoundsException.
    
    Bug:237989368
    
    Change-Id: Ica771f1ae4cb4ddddb7684bd964504ca9f31ea12
    
    Test: Monkey test
    
    str: rfp: merged on master
    CR: https://android-review.googlesource.com/c/platform/frameworks/base/+/2132452
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I129dfd2c89f795b8cd3d1eb723ab0fe067328b45
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- services/core/java/com/android/server/wm/RemoteAnimationController.java
+++ services/core/java/com/android/server/wm/RemoteAnimationController.java
@@ -73,6 +73,7 @@ class RemoteAnimationController implements DeathRecipient {
     final ArrayList<NonAppWindowAnimationAdapter> mPendingNonAppAnimations = new ArrayList<>();
     private final Handler mHandler;
     private final Runnable mTimeoutRunnable = () -> cancelAnimation("timeoutRunnable");
+    private boolean mIsFinishing;
 
     private FinishedCallback mFinishedCallback;
     private final boolean mIsActivityEmbedding;
@@ -303,6 +304,7 @@ class RemoteAnimationController implements DeathRecipient {
                 mPendingAnimations.size());
         mHandler.removeCallbacks(mTimeoutRunnable);
         synchronized (mService.mGlobalLock) {
+            mIsFinishing = true;
             unlinkToDeathOfRunner();
             releaseFinishedCallback();
             mService.openSurfaceTransaction();
@@ -347,6 +349,7 @@ class RemoteAnimationController implements DeathRecipient {
                 throw e;
             } finally {
                 mService.closeSurfaceTransaction("RemoteAnimationController#finished");
+                mIsFinishing = false;
             }
             // Reset input for all activities when the remote animation is finished.
             final Consumer<ActivityRecord> updateActivities =
@@ -592,6 +595,9 @@ class RemoteAnimationController implements DeathRecipient {
 
         @Override
         public void onAnimationCancelled(SurfaceControl animationLeash) {
+            if (mIsFinishing) {
+                return;
+            }
             if (mRecord.mAdapter == this) {
                 mRecord.mAdapter = null;
             } else {
