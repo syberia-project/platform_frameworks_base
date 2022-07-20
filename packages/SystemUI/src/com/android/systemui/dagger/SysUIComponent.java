763e970db28e0fe7cc47dad9107b6bb2c6b1c301
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2022-07-21 01:37:15 +0530

    SystemUI: Implement Reverse-engineered Smartspace
    From SQ3A.220605.009.A1
    
    [ghostrider-reborn]
    - Adapt to aosp, fix crash on click, misc cleanup
    
    Change-Id: I39444dd95621ccfbe1dfd737e357145014e440d0
    Co-authored-by: Anay Wadhera <awadhera@berkeley.edu>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SysUIComponent.java
@@ -42,6 +42,8 @@ import com.android.wm.shell.sysui.ShellInterface;
 import com.android.wm.shell.taskview.TaskViewFactory;
 import com.android.wm.shell.transition.ShellTransitions;
 
+import com.google.android.systemui.smartspace.KeyguardSmartspaceController;
+
 import dagger.BindsInstance;
 import dagger.Subcomponent;
 
@@ -173,4 +175,10 @@ public interface SysUIComponent {
      * Member injection into the supplied argument.
      */
     void inject(PeopleProvider peopleProvider);
+
+    /**
+     * Creates a KeyguardSmartspaceController.
+     */
+    @SysUISingleton
+    KeyguardSmartspaceController createKeyguardSmartspaceController();
 }
