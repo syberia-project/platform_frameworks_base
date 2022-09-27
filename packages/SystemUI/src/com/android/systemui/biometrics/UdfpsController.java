yanzszs/android_frameworks_base

Squashed patches for UDFPS customizations
* for future picking purposes
* added missing authors

Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
Co-authored-by: daniml3 <danimoral1001@gmail.com>
Co-authored-by: DennySPb <dennyspb@gmail.com>
Co-authored-by: kondors1995 <normandija1945@gmail.com>
Co-authored-by: AnierinB <anierinb@evolution-x.org>
Co-authored-by: 00day0 <therandomuser11@gmail.com>
Co-authored-by: Raaj52 <mail2raaj52@gmail.com>
Co-authored-by: Alexandru Scurtu <sasha.scurtu@gmail.com>
Co-authored-by: Anushek <anushekprasal@gmail.com>
Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
Co-authored-by: Hikari-no-Tenshi <Hikari-no-Tenshi@users.noreply.github.com>
Co-authored-by: TheScarastic <warabhishek@gmail.com>
Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>
Co-authored-by: nullxception <nullxception@users.noreply.github.com>
Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: Akito Mizukito <akito@evolution-x.org>
Co-authored-by: Marco Rizzo <marco.rizzo.00@outlook.com>
Co-authored-by: Varun Date <date.varun123@gmail.com>
Co-authored-by: firebird11 <hbgassel@gmail.com>
Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
Co-authored-by: spkal01 <kalligeross@gmail.com>
Co-authored-by: Joey Huab <joey@evolution-x.org>
Signed-off-by: str-br <alexfinhart@gmail.com>

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -18,7 +18,7 @@ package com.android.systemui.biometrics;
 
 import static android.hardware.biometrics.BiometricFingerprintConstants.FINGERPRINT_ACQUIRED_GOOD;
 import static android.hardware.biometrics.BiometricOverlayConstants.REASON_AUTH_KEYGUARD;
-
+import android.hardware.biometrics.BiometricOverlayConstants;
 import static android.hardware.biometrics.BiometricFingerprintConstants.FINGERPRINT_ACQUIRED_VENDOR;
 
 import static com.android.internal.util.Preconditions.checkNotNull;
@@ -39,6 +39,7 @@ import android.hardware.display.DisplayManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.hardware.fingerprint.IUdfpsOverlayController;
 import android.hardware.fingerprint.IUdfpsOverlayControllerCallback;
+import android.hardware.fingerprint.FingerprintSensorPropertiesInternal;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.PowerManager;
@@ -59,6 +60,7 @@ import android.view.accessibility.AccessibilityManager;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.util.LatencyTracker;
+import com.android.internal.util.rice.RiceUtils;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.systemui.R;
 import com.android.systemui.animation.ActivityLaunchAnimator;
@@ -149,7 +151,7 @@ public class UdfpsController implements DozeReceiver {
     // TODO(b/229290039): UDFPS controller should manage its dimensions on its own. Remove this.
     @Nullable private Runnable mAuthControllerUpdateUdfpsLocation;
     @Nullable private final AlternateUdfpsTouchProvider mAlternateTouchProvider;
-
+    @VisibleForTesting final FingerprintSensorPropertiesInternal mSensorProps;
     // Tracks the velocity of a touch to help filter out the touches that move too fast.
     @Nullable private VelocityTracker mVelocityTracker;
     // The ID of the pointer for which ACTION_DOWN has occurred. -1 means no pointer is active.
@@ -183,6 +185,7 @@ public class UdfpsController implements DozeReceiver {
     private final AmbientDisplayConfiguration mAmbientDisplayConfiguration;
     private final SecureSettings mSecureSettings;
     private boolean mScreenOffFod;
+    private UdfpsAnimation mUdfpsAnimation;
 
     @VisibleForTesting
     public static final VibrationAttributes VIBRATION_ATTRIBUTES =
@@ -680,6 +683,7 @@ public class UdfpsController implements DozeReceiver {
         mActivityLaunchAnimator = activityLaunchAnimator;
         mAlternateTouchProvider = aternateTouchProvider.orElse(null);
         mBiometricExecutor = biometricsExecutor;
+        mSensorProps = findFirstUdfps();
 
         mOrientationListener = new BiometricDisplayListener(
                 context,
@@ -717,6 +721,9 @@ public class UdfpsController implements DozeReceiver {
                 }
             }
         );
+        if (RiceUtils.isPackageInstalled(mContext, "com.rice.udfps.animations")) {
+            mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps);
+        }
     }
 
     private void updateScreenOffFodState() {
@@ -738,6 +745,17 @@ public class UdfpsController implements DozeReceiver {
                     "udfps-onStart-click",
                     VIBRATION_ATTRIBUTES);
         }
+    }
+
+    @Nullable
+    private FingerprintSensorPropertiesInternal findFirstUdfps() {
+        for (FingerprintSensorPropertiesInternal props :
+                mFingerprintManager.getSensorPropertiesInternal()) {
+            if (props.isAnyUdfpsType()) {
+                return props;
+            }
+        }
+        return null;
     }
 
     @Override
@@ -763,6 +781,12 @@ public class UdfpsController implements DozeReceiver {
 
         mOverlay = overlay;
         final int requestReason = overlay.getRequestReason();
+
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.setIsKeyguard(requestReason ==
+                    BiometricOverlayConstants.REASON_AUTH_KEYGUARD);
+        }
+
         if (requestReason == REASON_AUTH_KEYGUARD
                 && !mKeyguardUpdateMonitor.isFingerprintDetectionRunning()) {
             Log.d(TAG, "Attempting to showUdfpsOverlay when fingerprint detection"
@@ -929,6 +953,9 @@ public class UdfpsController implements DozeReceiver {
         for (Callback cb : mCallbacks) {
             cb.onFingerDown();
         }
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.show();
+        }
     }
 
     private void onFingerUp(long requestId, @NonNull UdfpsView view) {
@@ -946,6 +973,9 @@ public class UdfpsController implements DozeReceiver {
             for (Callback cb : mCallbacks) {
                 cb.onFingerUp();
             }
+        }
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.hide();
         }
         mOnFingerDown = false;
         if (view.isIlluminationRequested()) {

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsKeyguardView.java
@@ -26,6 +26,7 @@ import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.MathUtils;
 import android.view.View;
@@ -37,6 +38,8 @@ import androidx.annotation.Nullable;
 import androidx.asynclayoutinflater.view.AsyncLayoutInflater;
 
 import android.graphics.Color;
+import com.android.internal.util.rice.RiceUtils;
+
 import com.android.systemui.R;
 import com.android.systemui.animation.Interpolators;
 
@@ -75,6 +78,9 @@ public class UdfpsKeyguardView extends UdfpsAnimationView {
     private int mAnimationType = ANIMATION_NONE;
     private boolean mFullyInflated;
 
+    private boolean mCustomUdfpsIcon;
+    private boolean mPackageInstalled;
+
     public UdfpsKeyguardView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         mFingerprintDrawable = new UdfpsFpDrawable(context);
@@ -83,6 +89,9 @@ public class UdfpsKeyguardView extends UdfpsAnimationView {
             .getDimensionPixelSize(R.dimen.udfps_burn_in_offset_x);
         mMaxBurnInOffsetY = context.getResources()
             .getDimensionPixelSize(R.dimen.udfps_burn_in_offset_y);
+
+        mPackageInstalled = com.android.internal.util.rice.RiceUtils.isPackageInstalled(
+                mContext, "com.rice.udfps.icons");
     }
 
     @Override
@@ -93,6 +102,7 @@ public class UdfpsKeyguardView extends UdfpsAnimationView {
         AsyncLayoutInflater inflater = new AsyncLayoutInflater(mContext);
         inflater.inflate(R.layout.udfps_keyguard_view_internal, this,
                 mLayoutInflaterFinishListener);
+
     }
 
     @Override
@@ -112,6 +122,14 @@ public class UdfpsKeyguardView extends UdfpsAnimationView {
     public boolean dozeTimeTick() {
         updateBurnInOffsets();
         return true;
+    }
+
+    private void updateIcon() {
+        mCustomUdfpsIcon = mPackageInstalled && (Settings.System.getInt(
+                mContext.getContentResolver(), Settings.System.UDFPS_ICON, 0) != 0);
+        mBgProtection.setImageDrawable(mCustomUdfpsIcon
+                ? mFingerprintDrawable :
+                getContext().getDrawable(R.drawable.fingerprint_bg));
     }
 
     private void updateBurnInOffsets() {
@@ -135,12 +153,14 @@ public class UdfpsKeyguardView extends UdfpsAnimationView {
             mLockScreenFp.setTranslationX(mBurnInOffsetX);
             mLockScreenFp.setTranslationY(mBurnInOffsetY);
             mBgProtection.setAlpha(1f - mInterpolatedDarkAmount);
-            mLockScreenFp.setAlpha(1f - mInterpolatedDarkAmount);
+            mLockScreenFp.setAlpha(mCustomUdfpsIcon ? 0.0f
+                : (1f - mInterpolatedDarkAmount));
         } else if (darkAmountForAnimation == 0f) {
             mLockScreenFp.setTranslationX(0);
             mLockScreenFp.setTranslationY(0);
             mBgProtection.setAlpha(mAlpha / 255f);
-            mLockScreenFp.setAlpha(mAlpha / 255f);
+            mLockScreenFp.setAlpha(mCustomUdfpsIcon ? 0.0f
+                : (mAlpha / 255f));
         } else {
             mBgProtection.setAlpha(0f);
             mLockScreenFp.setAlpha(0f);
@@ -174,7 +194,6 @@ public class UdfpsKeyguardView extends UdfpsAnimationView {
         }
 
         mTextColorPrimary = Color.parseColor("#FFFFFF");
-        mBgProtection.setImageDrawable(getContext().getDrawable(R.drawable.fingerprint_bg));
         mLockScreenFp.invalidate(); // updated with a valueCallback
     }
 
@@ -290,6 +309,7 @@ public class UdfpsKeyguardView extends UdfpsAnimationView {
             updatePadding();
             updateColor();
             updateAlpha();
+            updateIcon();
             parent.addView(view);
 
             // requires call to invalidate to update the color

syberia-project/platform_frameworks_base

Signed-off-by: DennySPb <dennyspb@gmail.com>

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -19,6 +19,8 @@ package com.android.systemui.biometrics;
 import static android.hardware.biometrics.BiometricFingerprintConstants.FINGERPRINT_ACQUIRED_GOOD;
 import static android.hardware.biometrics.BiometricOverlayConstants.REASON_AUTH_KEYGUARD;
 
+import android.hardware.biometrics.BiometricOverlayConstants;
+
 import static com.android.internal.util.Preconditions.checkNotNull;
 import static com.android.systemui.classifier.Classifier.UDFPS_AUTHENTICATION;
 
@@ -34,6 +36,7 @@ import android.hardware.display.DisplayManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.hardware.fingerprint.IUdfpsOverlayController;
 import android.hardware.fingerprint.IUdfpsOverlayControllerCallback;
+import android.hardware.fingerprint.FingerprintSensorPropertiesInternal;
 import android.os.Handler;
 import android.os.PowerManager;
 import android.os.Process;
@@ -51,6 +54,7 @@ import android.view.accessibility.AccessibilityManager;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.util.LatencyTracker;
+import com.android.internal.util.syberia.SyberiaUtils;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.systemui.animation.ActivityLaunchAnimator;
 import com.android.systemui.biometrics.dagger.BiometricsBackground;
@@ -138,7 +142,7 @@ public class UdfpsController implements DozeReceiver {
     // TODO(b/229290039): UDFPS controller should manage its dimensions on its own. Remove this.
     @Nullable private Runnable mAuthControllerUpdateUdfpsLocation;
     @Nullable private final AlternateUdfpsTouchProvider mAlternateTouchProvider;
-
+    @VisibleForTesting final FingerprintSensorPropertiesInternal mSensorProps;
     // Tracks the velocity of a touch to help filter out the touches that move too fast.
     @Nullable private VelocityTracker mVelocityTracker;
     // The ID of the pointer for which ACTION_DOWN has occurred. -1 means no pointer is active.
@@ -165,6 +169,8 @@ public class UdfpsController implements DozeReceiver {
     private boolean mOnFingerDown;
     private boolean mAttemptedToDismissKeyguard;
     private final Set<Callback> mCallbacks = new HashSet<>();
+
+    private UdfpsAnimation mUdfpsAnimation;
 
     @VisibleForTesting
     public static final VibrationAttributes VIBRATION_ATTRIBUTES =
@@ -640,6 +646,7 @@ public class UdfpsController implements DozeReceiver {
         mActivityLaunchAnimator = activityLaunchAnimator;
         mAlternateTouchProvider = aternateTouchProvider.orElse(null);
         mBiometricExecutor = biometricsExecutor;
+        mSensorProps = findFirstUdfps();
 
         mOrientationListener = new BiometricDisplayListener(
                 context,
@@ -663,7 +670,11 @@ public class UdfpsController implements DozeReceiver {
 
         udfpsHapticsSimulator.setUdfpsController(this);
         udfpsShell.setUdfpsOverlayController(mUdfpsOverlayController);
+        if (SyberiaUtils.isPackageInstalled(mContext, "com.syberia.udfps.resources")) {
+            mUdfpsAnimation = new UdfpsAnimation(mContext, mWindowManager, mSensorProps);
+        }
     }
+
 
     /**
      * If a11y touchExplorationEnabled, play haptic to signal UDFPS scanning started.
@@ -678,6 +689,17 @@ public class UdfpsController implements DozeReceiver {
                     "udfps-onStart-click",
                     VIBRATION_ATTRIBUTES);
         }
+    }
+
+    @Nullable
+    private FingerprintSensorPropertiesInternal findFirstUdfps() {
+        for (FingerprintSensorPropertiesInternal props :
+                mFingerprintManager.getSensorPropertiesInternal()) {
+            if (props.isAnyUdfpsType()) {
+                return props;
+            }
+        }
+        return null;
     }
 
     @Override
@@ -703,6 +725,12 @@ public class UdfpsController implements DozeReceiver {
 
         mOverlay = overlay;
         final int requestReason = overlay.getRequestReason();
+
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.setIsKeyguard(requestReason ==
+                    BiometricOverlayConstants.REASON_AUTH_KEYGUARD);
+        }
+
         if (requestReason == REASON_AUTH_KEYGUARD
                 && !mKeyguardUpdateMonitor.isFingerprintDetectionRunning()) {
             Log.d(TAG, "Attempting to showUdfpsOverlay when fingerprint detection"
@@ -869,6 +897,9 @@ public class UdfpsController implements DozeReceiver {
         for (Callback cb : mCallbacks) {
             cb.onFingerDown();
         }
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.show();
+        }
     }
 
     private void onFingerUp(long requestId, @NonNull UdfpsView view) {
@@ -886,6 +917,9 @@ public class UdfpsController implements DozeReceiver {
             for (Callback cb : mCallbacks) {
                 cb.onFingerUp();
             }
+        }
+        if (mUdfpsAnimation != null) {
+            mUdfpsAnimation.hide();
         }
         mOnFingerDown = false;
         if (view.isIlluminationRequested()) {

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsKeyguardView.java
@@ -26,6 +26,7 @@ import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.MathUtils;
 import android.view.View;
@@ -37,6 +38,8 @@ import androidx.annotation.Nullable;
 import androidx.asynclayoutinflater.view.AsyncLayoutInflater;
 
 import com.android.settingslib.Utils;
+import com.android.internal.util.syberia.SyberiaUtils;
+
 import com.android.systemui.R;
 import com.android.systemui.animation.Interpolators;
 
@@ -75,6 +78,9 @@ public class UdfpsKeyguardView extends UdfpsAnimationView {
     private int mAnimationType = ANIMATION_NONE;
     private boolean mFullyInflated;
 
+    private boolean mCustomUdfpsIcon;
+    private boolean mPackageInstalled;
+
     public UdfpsKeyguardView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         mFingerprintDrawable = new UdfpsFpDrawable(context);
@@ -83,6 +89,9 @@ public class UdfpsKeyguardView extends UdfpsAnimationView {
             .getDimensionPixelSize(R.dimen.udfps_burn_in_offset_x);
         mMaxBurnInOffsetY = context.getResources()
             .getDimensionPixelSize(R.dimen.udfps_burn_in_offset_y);
+
+        mPackageInstalled = com.android.internal.util.syberia.SyberiaUtils.isPackageInstalled(
+                mContext, "com.syberia.udfps.resources");
     }
 
     @Override
@@ -93,6 +102,7 @@ public class UdfpsKeyguardView extends UdfpsAnimationView {
         AsyncLayoutInflater inflater = new AsyncLayoutInflater(mContext);
         inflater.inflate(R.layout.udfps_keyguard_view_internal, this,
                 mLayoutInflaterFinishListener);
+
     }
 
     @Override
@@ -112,6 +122,14 @@ public class UdfpsKeyguardView extends UdfpsAnimationView {
     public boolean dozeTimeTick() {
         updateBurnInOffsets();
         return true;
+    }
+
+    private void updateIcon() {
+        mCustomUdfpsIcon = mPackageInstalled && (Settings.System.getInt(
+                mContext.getContentResolver(), Settings.System.UDFPS_ICON, 0) != 0);
+        mBgProtection.setImageDrawable(mCustomUdfpsIcon
+                ? mFingerprintDrawable :
+                getContext().getDrawable(R.drawable.fingerprint_bg));
     }
 
     private void updateBurnInOffsets() {
@@ -135,12 +153,14 @@ public class UdfpsKeyguardView extends UdfpsAnimationView {
             mLockScreenFp.setTranslationX(mBurnInOffsetX);
             mLockScreenFp.setTranslationY(mBurnInOffsetY);
             mBgProtection.setAlpha(1f - mInterpolatedDarkAmount);
-            mLockScreenFp.setAlpha(1f - mInterpolatedDarkAmount);
+            mLockScreenFp.setAlpha(mCustomUdfpsIcon ? 0.0f
+                : (1f - mInterpolatedDarkAmount));
         } else if (darkAmountForAnimation == 0f) {
             mLockScreenFp.setTranslationX(0);
             mLockScreenFp.setTranslationY(0);
             mBgProtection.setAlpha(mAlpha / 255f);
-            mLockScreenFp.setAlpha(mAlpha / 255f);
+            mLockScreenFp.setAlpha(mCustomUdfpsIcon ? 0.0f
+                : (mAlpha / 255f));
         } else {
             mBgProtection.setAlpha(0f);
             mLockScreenFp.setAlpha(0f);
@@ -175,7 +195,6 @@ public class UdfpsKeyguardView extends UdfpsAnimationView {
 
         mTextColorPrimary = Utils.getColorAttrDefaultColor(mContext,
             android.R.attr.textColorPrimary);
-        mBgProtection.setImageDrawable(getContext().getDrawable(R.drawable.fingerprint_bg));
         mLockScreenFp.invalidate(); // updated with a valueCallback
     }
 
@@ -291,6 +310,7 @@ public class UdfpsKeyguardView extends UdfpsAnimationView {
             updatePadding();
             updateColor();
             updateAlpha();
+            updateIcon();
             parent.addView(view);
 
             // requires call to invalidate to update the color