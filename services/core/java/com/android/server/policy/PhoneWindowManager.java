2b7e7c29dea1c9a4da98f5e8af4968cce57478db
Ido Ben-Hur <idoybh2@gmail.com>
2022-12-10 12:54:11 +0200

    base: Pocket Judge: Don't block dialer UI
    What happens is that the dialer looses focus and stops the ringer / vibration
    Listen to calls and never display when we are in one
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -5253,6 +5253,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             return;
         }
 
+        TelecomManager telecomManager = getTelecommService();
+        if (telecomManager != null && telecomManager.isInCall()) {
+            return;
+        }
+
         if (mPowerManager.isInteractive() && !isKeyguardShowingAndNotOccluded()){
             return;
         }
