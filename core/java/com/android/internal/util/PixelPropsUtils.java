0cdf19fc3f68d62d7006158ff2980499ee5a8723
DennySPb <dennyspb@gmail.com>
2022-12-16 14:15:49 +0300

    PixelProps: update redfin fp to dec 2022
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I5a1b944b362ecbd38dfbdb8a7589de539f83762d

--- core/java/com/android/internal/util/PixelPropsUtils.java
+++ core/java/com/android/internal/util/PixelPropsUtils.java
@@ -42,7 +42,7 @@ public class PixelPropsUtils {
         "DEVICE", "redfin",
         "PRODUCT", "redfin",
         "MODEL", "Pixel 5",
-        "FINGERPRINT", "google/redfin/redfin:13/TP1A.220624.014/8819323:user/release-keys"
+        "FINGERPRINT", "google/redfin/redfin:13/TQ1A.221205.011/9244662:user/release-keys"
     );
 
     private static final Map<String, Object> sPixelXLProps = Map.of(
