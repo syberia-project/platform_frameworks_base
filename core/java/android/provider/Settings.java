81b1b1fea982fdf637307088bff3e1a65e5125f3
DennySPb <dennyspb@gmail.com>
2023-01-09 14:00:08 +0300

    Settings: Make cursor volume control string readable for all
    fixes crash for some 3d party keyboards
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Iecc836c5b35b553d8032ab1fdc8bd1336e4d5fb8
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5814,6 +5814,7 @@ public final class Settings {
           * 2 - Volume up/down moves cursor right/left
           * @hide
           */
+        @Readable
         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
