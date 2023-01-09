3ece3681f5ca47458402818b55d36486d75ef408
Ido Ben-Hur <idoybh2@gmail.com>
2023-01-09 03:38:02 +0200

    base: Pocket Judge: Don't go to sleep while on call
    This causes gdialer to stop vibrations. Let's not.
    
    Also:
    * Properly resume listening on fingerprints
    * Use TYPE_SECURE_SYSTEM_OVERLAY for the pocket lock view to actually overlay above everything
      (see com.android.server.policy.WindowManagerPolicy for a full list of priorities)
    
    This commit also reverts previous temp solutions to this issue:
    
    Revert "base: Allow enabling pocket judge on call via an overlay"
    
    This reverts commit 0e01d73876740ebc7a182b8c334abec243f67c18.
    
    Revert "base: Pocket Judge: Don't block dialer UI"
    
    This reverts commit 3e1510a076fca97d06bdefd46e11795b118cc5f5.
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I567381d7805a2c3917fca027d3df4e5f6ed508a4
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -738,7 +738,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private PocketLock mPocketLock;
     private boolean mPocketLockShowing;
     private boolean mIsDeviceInPocket;
-    private boolean mIsPocketDisabledOnCall;
     private final IPocketCallback mPocketCallback = new IPocketCallback.Stub() {
 
         @Override
@@ -2340,8 +2339,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 res.getBoolean(com.android.internal.R.bool.config_wakeOnAssistKeyPress);
         mWakeOnBackKeyPress =
                 res.getBoolean(com.android.internal.R.bool.config_wakeOnBackKeyPress);
-        mIsPocketDisabledOnCall =
-                res.getBoolean(com.android.internal.R.bool.config_pocketJudgeDisableOnCall);
 
         // Double-tap-to-doze
         mNativeDoubleTapToDozeAvailable = !TextUtils.isEmpty(
@@ -6152,13 +6149,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (!mSystemReady || !mSystemBooted || !mKeyguardDrawnOnce
                 || mPocketLock == null || mPocketLockShowing) {
             return;
-        }
-
-        if (mIsPocketDisabledOnCall) {
-            TelecomManager telecomManager = getTelecommService();
-            if (telecomManager != null && telecomManager.isInCall()) {
-                return;
-            }
         }
 
         if (mPowerManager.isInteractive() && !isKeyguardShowingAndNotOccluded()){
