68a9def884c4074b59425e9f9ed53258f19ed3f6
Pranav Vashi <neobuddy89@gmail.com>
2023-02-26 16:57:25 +0530

    AppLock: Allow locking all apps visible in launcher
    Change-Id: Id61c0c5d505711d0ad91f172d2bdc709cde3632c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/syberia/SyberiaUtils.java
+++ core/java/com/android/internal/util/syberia/SyberiaUtils.java
@@ -107,6 +107,24 @@ public class SyberiaUtils {
         return isPackageInstalled(context, pkg, true);
     }
 
+    public static List<String> launchablePackages(Context context) {
+        List<String> list = new ArrayList<>();
+
+        Intent filter = new Intent(Intent.ACTION_MAIN, null);
+        filter.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> apps = context.getPackageManager().queryIntentActivities(filter,
+                PackageManager.GET_META_DATA);
+
+        int numPackages = apps.size();
+        for (int i = 0; i < numPackages; i++) {
+            ResolveInfo app = apps.get(i);
+            list.add(app.activityInfo.packageName);
+        }
+
+        return list;
+    }
+
     public static boolean deviceHasFlashlight(Context ctx) {
         return ctx.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
     }
