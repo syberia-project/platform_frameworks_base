85563f83eeca7b26629adb0d2eb048d1ca82aa1c
Jake Weinstein <jake@aospa.co>
2023-03-21 11:30:14 +0900

    SystemUI: Forward port QS fling animation boost to NotificationPanelViewController
    PanelViewController.java was deleted and migrated
    to NotificationPanelViewController.java in Android
    13 QPR2.
    
    Change-Id: I2113e059709da2154479d9a6a4a3d5100f829a52

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -70,6 +70,7 @@ import android.os.Trace;
 import android.os.UserManager;
 import android.os.VibrationEffect;
 import android.provider.Settings;
+import android.util.BoostFramework;
 import android.util.IndentingPrintWriter;
 import android.util.Log;
 import android.util.MathUtils;
@@ -644,6 +645,11 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
     private NotificationStackScrollLayout mNotificationStackScroller;
     private boolean mUseIslandNotification;
 
+    /**
+     * For PanelView fling perflock call
+     */
+    private BoostFramework mPerf = null;
+
     private final Runnable mFlingCollapseRunnable = () -> fling(0, false /* expand */,
             mNextCollapseSpeedUpFactor, false /* expandBecauseOfFalsing */);
     private final Runnable mAnimateKeyguardBottomAreaInvisibleEndRunnable =
@@ -1018,6 +1024,7 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
         mAlternateBouncerInteractor = alternateBouncerInteractor;
         mKeyguardRootView = keyguardRootView;
         dumpManager.registerDumpable(this);
+        mPerf = new BoostFramework();
     }
 
     private void unlockAnimationFinished() {
@@ -2144,6 +2151,11 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
                 }
             });
         }
+
+        if (mPerf != null) {
+            String currentPackage = mView.getContext().getPackageName();
+            mPerf.perfHint(BoostFramework.VENDOR_HINT_SCROLL_BOOST, currentPackage, -1, BoostFramework.Scroll.PANEL_VIEW);
+        }
         animator.addListener(new AnimatorListenerAdapter() {
             private boolean mCancelled;
 
@@ -2156,11 +2168,17 @@ public final class NotificationPanelViewController implements ShadeSurface, Dump
 
             @Override
             public void onAnimationCancel(Animator animation) {
+                if (mPerf != null) {
+                    mPerf.perfLockRelease();
+                }
                 mCancelled = true;
             }
 
             @Override
             public void onAnimationEnd(Animator animation) {
+                if (mPerf != null) {
+                    mPerf.perfLockRelease();
+                }
                 if (shouldSpringBack && !mCancelled) {
                     // After the shade is flung open to an overscrolled state, spring back
                     // the shade by reducing section padding to 0.
