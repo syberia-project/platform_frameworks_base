LineageOS/android_lineage-sdk

Introduce anti flicker mode in LiveDisplay
Change-Id: Iba7387ff16d463d087c9a38bced90b6c1bd503b1

sdk/src/java/lineageos/hardware/LineageHardwareManager.java
@@ -31,6 +31,7 @@ import com.android.internal.util.ArrayUtils;
 import lineageos.app.LineageContextConstants;
 
 import vendor.lineage.livedisplay.V2_0.IAdaptiveBacklight;
+import vendor.lineage.livedisplay.V2_0.IAntiFlicker;
 import vendor.lineage.livedisplay.V2_0.IAutoContrast;
 import vendor.lineage.livedisplay.V2_0.IColorBalance;
 import vendor.lineage.livedisplay.V2_0.IColorEnhancement;
@@ -158,8 +159,15 @@ public final class LineageHardwareManager {
     @VisibleForTesting
     public static final int FEATURE_TOUCHSCREEN_GESTURES = 0x80000;
 
+    /**
+     * Anti flicker mode
+     */
+    @VisibleForTesting
+    public static final int FEATURE_ANTI_FLICKER = 0x200000;
+
     private static final List<Integer> BOOLEAN_FEATURES = Arrays.asList(
         FEATURE_ADAPTIVE_BACKLIGHT,
+        FEATURE_ANTI_FLICKER,
         FEATURE_AUTO_CONTRAST,
         FEATURE_COLOR_ENHANCEMENT,
         FEATURE_HIGH_TOUCH_SENSITIVITY,
@@ -272,6 +280,8 @@ public final class LineageHardwareManager {
             switch (feature) {
                 case FEATURE_ADAPTIVE_BACKLIGHT:
                     return IAdaptiveBacklight.getService(true);
+                case FEATURE_ANTI_FLICKER:
+                    return IAntiFlicker.getService(true);
                 case FEATURE_AUTO_CONTRAST:
                     return IAutoContrast.getService(true);
                 case FEATURE_COLOR_BALANCE:
@@ -345,6 +355,9 @@ public final class LineageHardwareManager {
                     case FEATURE_ADAPTIVE_BACKLIGHT:
                         IAdaptiveBacklight adaptiveBacklight = (IAdaptiveBacklight) obj;
                         return adaptiveBacklight.isEnabled();
+                    case FEATURE_ANTI_FLICKER:
+                        IAntiFlicker antiFlicker = (IAntiFlicker) obj;
+                        return antiFlicker.isEnabled();
                     case FEATURE_AUTO_CONTRAST:
                         IAutoContrast autoContrast = (IAutoContrast) obj;
                         return autoContrast.isEnabled();
@@ -400,6 +413,9 @@ public final class LineageHardwareManager {
                     case FEATURE_ADAPTIVE_BACKLIGHT:
                         IAdaptiveBacklight adaptiveBacklight = (IAdaptiveBacklight) obj;
                         return adaptiveBacklight.setEnabled(enable);
+                    case FEATURE_ANTI_FLICKER:
+                        IAntiFlicker antiFlicker = (IAntiFlicker) obj;
+                        return antiFlicker.setEnabled(enable);
                     case FEATURE_AUTO_CONTRAST:
                         IAutoContrast autoContrast = (IAutoContrast) obj;
                         return autoContrast.setEnabled(enable);

lineage/lib/main/java/org/lineageos/platform/internal/display/DisplayHardwareController.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
- *               2018 The LineageOS Project
+ *               2018-2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,11 +50,13 @@ public class DisplayHardwareController extends LiveDisplayFeature {
     private final boolean mUseCABC;
     private final boolean mUseReaderMode;
     private final boolean mUseDisplayModes;
+    private final boolean mUseAntiFlicker;
 
     // default values
     private final boolean mDefaultAutoContrast;
     private final boolean mDefaultColorEnhancement;
     private final boolean mDefaultCABC;
+    private final boolean mDefaultAntiFlicker;
 
     // color adjustment holders
     private final float[] mAdditionalAdjustment = getDefaultAdjustment();
@@ -75,6 +77,8 @@ public class DisplayHardwareController extends LiveDisplayFeature {
             LineageSettings.System.getUriFor(LineageSettings.System.DISPLAY_CABC);
     private static final Uri DISPLAY_READING_MODE =
             LineageSettings.System.getUriFor(LineageSettings.System.DISPLAY_READING_MODE);
+    private static final Uri DISPLAY_ANTI_FLICKER =
+            LineageSettings.System.getUriFor(LineageSettings.System.DISPLAY_ANTI_FLICKER);
 
     public DisplayHardwareController(Context context, Handler handler) {
         super(context, handler);
@@ -104,6 +108,11 @@ public class DisplayHardwareController extends LiveDisplayFeature {
         mUseReaderMode = mHardware
                 .isSupported(LineageHardwareManager.FEATURE_READING_ENHANCEMENT);
 
+        mUseAntiFlicker = mHardware
+                .isSupported(LineageHardwareManager.FEATURE_ANTI_FLICKER);
+        mDefaultAntiFlicker = mContext.getResources().getBoolean(
+                org.lineageos.platform.internal.R.bool.config_defaultAntiFlicker);
+
         if (mUseColorAdjustment) {
             mMaxColor = mHardware.getDisplayColorCalibrationMax();
             copyColors(getColorAdjustment(), mColorAdjustment);
@@ -130,6 +139,9 @@ public class DisplayHardwareController extends LiveDisplayFeature {
         }
         if (mUseReaderMode) {
             settings.add(DISPLAY_READING_MODE);
+        }
+        if (mUseAntiFlicker) {
+            settings.add(DISPLAY_ANTI_FLICKER);
         }
 
         if (settings.size() == 0) {
@@ -159,8 +171,11 @@ public class DisplayHardwareController extends LiveDisplayFeature {
         if (mUseReaderMode) {
             caps.set(LiveDisplayManager.FEATURE_READING_ENHANCEMENT);
         }
+        if (mUseAntiFlicker) {
+            caps.set(LiveDisplayManager.FEATURE_ANTI_FLICKER);
+        }
         return mUseAutoContrast || mUseColorEnhancement || mUseCABC || mUseColorAdjustment ||
-            mUseDisplayModes || mUseReaderMode;
+            mUseDisplayModes || mUseReaderMode || mUseAntiFlicker;
     }
 
     @Override
@@ -178,6 +193,9 @@ public class DisplayHardwareController extends LiveDisplayFeature {
             copyColors(getColorAdjustment(), mColorAdjustment);
             updateColorAdjustment();
         }
+        if (uri == null || uri.equals(DISPLAY_ANTI_FLICKER)) {
+            updateAntiFlicker();
+        }
     }
 
     private synchronized void updateHardware() {
@@ -185,6 +203,7 @@ public class DisplayHardwareController extends LiveDisplayFeature {
             updateCABCMode();
             updateAutoContrast();
             updateColorEnhancement();
+            updateAntiFlicker();
         }
     }
 
@@ -214,6 +233,7 @@ public class DisplayHardwareController extends LiveDisplayFeature {
         pw.println("  mUseCABC=" + mUseCABC);
         pw.println("  mUseDisplayModes=" + mUseDisplayModes);
         pw.println("  mUseReaderMode=" + mUseReaderMode);
+        pw.println("  mUseAntiFlicker=" + mUseAntiFlicker);
         pw.println();
         pw.println("  DisplayHardwareController State:");
         pw.println("    mAutoContrast=" + isAutoContrastEnabled());
@@ -274,6 +294,16 @@ public class DisplayHardwareController extends LiveDisplayFeature {
         if (validateColors(rgb)) {
             animateDisplayColor(rgb);
         }
+    }
+
+    /**
+     * Anti flicker mode
+     */
+    private void updateAntiFlicker() {
+        if (!mUseAntiFlicker) {
+            return;
+        }
+        mHardware.set(LineageHardwareManager.FEATURE_ANTI_FLICKER, isAntiFlickerEnabled());
     }
 
     /**
@@ -508,5 +538,18 @@ public class DisplayHardwareController extends LiveDisplayFeature {
             dst[1] = src[1];
             dst[2] = src[2];
         }
+    }
+
+    boolean isAntiFlickerEnabled() {
+        return mUseAntiFlicker &&
+                getBoolean(LineageSettings.System.DISPLAY_ANTI_FLICKER, mDefaultAntiFlicker);
+    }
+
+    boolean setAntiFlickerEnabled(boolean enabled) {
+        if (!mUseAntiFlicker) {
+            return false;
+        }
+        putBoolean(LineageSettings.System.DISPLAY_ANTI_FLICKER, enabled);
+        return true;
     }
 }

syberia-project/platform_frameworks_base

Signed-off-by: DennySPb <dennyspb@gmail.com>

core/java/com/android/internal/custom/hardware/LineageHardwareManager.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015-2016 The CyanogenMod Project
- *               2017-2019 The LineageOS Project
+ *               2017-2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,6 +42,7 @@ import vendor.lineage.livedisplay.V2_0.IDisplayModes;
 import vendor.lineage.livedisplay.V2_0.IPictureAdjustment;
 import vendor.lineage.livedisplay.V2_0.IReadingEnhancement;
 import vendor.lineage.livedisplay.V2_0.ISunlightEnhancement;
+import vendor.lineage.livedisplay.V2_1.IAntiFlicker;
 
 import java.io.UnsupportedEncodingException;
 import java.lang.IllegalArgumentException;
@@ -123,8 +124,15 @@ public final class LineageHardwareManager {
     @VisibleForTesting
     public static final int FEATURE_PICTURE_ADJUSTMENT = 0x1000;
 
+    /**
+     * Anti flicker mode
+     */
+    @VisibleForTesting
+    public static final int FEATURE_ANTI_FLICKER = 0x200000;
+
     private static final List<Integer> BOOLEAN_FEATURES = Arrays.asList(
         FEATURE_ADAPTIVE_BACKLIGHT,
+        FEATURE_ANTI_FLICKER,
         FEATURE_AUTO_CONTRAST,
         FEATURE_COLOR_ENHANCEMENT,
         FEATURE_SUNLIGHT_ENHANCEMENT,
@@ -232,6 +240,8 @@ public final class LineageHardwareManager {
             switch (feature) {
                 case FEATURE_ADAPTIVE_BACKLIGHT:
                     return IAdaptiveBacklight.getService(true);
+                case FEATURE_ANTI_FLICKER:
+                    return IAntiFlicker.getService(true);
                 case FEATURE_AUTO_CONTRAST:
                     return IAutoContrast.getService(true);
                 case FEATURE_COLOR_BALANCE:
@@ -295,6 +305,9 @@ public final class LineageHardwareManager {
                     case FEATURE_ADAPTIVE_BACKLIGHT:
                         IAdaptiveBacklight adaptiveBacklight = (IAdaptiveBacklight) obj;
                         return adaptiveBacklight.isEnabled();
+                    case FEATURE_ANTI_FLICKER:
+                        IAntiFlicker antiFlicker = (IAntiFlicker) obj;
+                        return antiFlicker.isEnabled();
                     case FEATURE_AUTO_CONTRAST:
                         IAutoContrast autoContrast = (IAutoContrast) obj;
                         return autoContrast.isEnabled();
@@ -338,6 +351,9 @@ public final class LineageHardwareManager {
                     case FEATURE_ADAPTIVE_BACKLIGHT:
                         IAdaptiveBacklight adaptiveBacklight = (IAdaptiveBacklight) obj;
                         return adaptiveBacklight.setEnabled(enable);
+                    case FEATURE_ANTI_FLICKER:
+                        IAntiFlicker antiFlicker = (IAntiFlicker) obj;
+                        return antiFlicker.setEnabled(enable);
                     case FEATURE_AUTO_CONTRAST:
                         IAutoContrast autoContrast = (IAutoContrast) obj;
                         return autoContrast.setEnabled(enable);

services/core/java/com/android/server/custom/display/DisplayHardwareController.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
- *               2018 The LineageOS Project
+ *               2018-2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,11 +50,13 @@ public class DisplayHardwareController extends LiveDisplayFeature {
     private final boolean mUseCABC;
     private final boolean mUseReaderMode;
     private final boolean mUseDisplayModes;
+    private final boolean mUseAntiFlicker;
 
     // default values
     private final boolean mDefaultAutoContrast;
     private final boolean mDefaultColorEnhancement;
     private final boolean mDefaultCABC;
+    private final boolean mDefaultAntiFlicker;
 
     // color adjustment holders
     private final float[] mAdditionalAdjustment = getDefaultAdjustment();
@@ -75,6 +77,8 @@ public class DisplayHardwareController extends LiveDisplayFeature {
             Settings.System.getUriFor(Settings.System.DISPLAY_CABC);
     private static final Uri DISPLAY_READING_MODE =
             Settings.System.getUriFor(Settings.System.DISPLAY_READING_MODE);
+    private static final Uri DISPLAY_ANTI_FLICKER =
+            Settings.System.getUriFor(Settings.System.DISPLAY_ANTI_FLICKER);
 
     public DisplayHardwareController(Context context, Handler handler) {
         super(context, handler);
@@ -104,6 +108,11 @@ public class DisplayHardwareController extends LiveDisplayFeature {
         mUseReaderMode = mHardware
                 .isSupported(LineageHardwareManager.FEATURE_READING_ENHANCEMENT);
 
+        mUseAntiFlicker = mHardware
+                .isSupported(LineageHardwareManager.FEATURE_ANTI_FLICKER);
+        mDefaultAntiFlicker = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_defaultAntiFlicker);
+
         if (mUseColorAdjustment) {
             mMaxColor = mHardware.getDisplayColorCalibrationMax();
             copyColors(getColorAdjustment(), mColorAdjustment);
@@ -130,6 +139,9 @@ public class DisplayHardwareController extends LiveDisplayFeature {
         }
         if (mUseReaderMode) {
             settings.add(DISPLAY_READING_MODE);
+        }
+        if (mUseAntiFlicker) {
+            settings.add(DISPLAY_ANTI_FLICKER);
         }
 
         if (settings.size() == 0) {
@@ -159,8 +171,11 @@ public class DisplayHardwareController extends LiveDisplayFeature {
         if (mUseReaderMode) {
             caps.set(LiveDisplayManager.FEATURE_READING_ENHANCEMENT);
         }
+        if (mUseAntiFlicker) {
+            caps.set(LiveDisplayManager.FEATURE_ANTI_FLICKER);
+        }
         return mUseAutoContrast || mUseColorEnhancement || mUseCABC || mUseColorAdjustment ||
-            mUseDisplayModes || mUseReaderMode;
+            mUseDisplayModes || mUseReaderMode || mUseAntiFlicker;
     }
 
     @Override
@@ -178,6 +193,9 @@ public class DisplayHardwareController extends LiveDisplayFeature {
             copyColors(getColorAdjustment(), mColorAdjustment);
             updateColorAdjustment();
         }
+        if (uri == null || uri.equals(DISPLAY_ANTI_FLICKER)) {
+            updateAntiFlicker();
+        }
     }
 
     private synchronized void updateHardware() {
@@ -185,6 +203,7 @@ public class DisplayHardwareController extends LiveDisplayFeature {
             updateCABCMode();
             updateAutoContrast();
             updateColorEnhancement();
+            updateAntiFlicker();
         }
     }
 
@@ -214,6 +233,7 @@ public class DisplayHardwareController extends LiveDisplayFeature {
         pw.println("  mUseCABC=" + mUseCABC);
         pw.println("  mUseDisplayModes=" + mUseDisplayModes);
         pw.println("  mUseReaderMode=" + mUseReaderMode);
+        pw.println("  mUseAntiFlicker=" + mUseAntiFlicker);
         pw.println();
         pw.println("  DisplayHardwareController State:");
         pw.println("    mAutoContrast=" + isAutoContrastEnabled());
@@ -274,6 +294,16 @@ public class DisplayHardwareController extends LiveDisplayFeature {
         if (validateColors(rgb)) {
             animateDisplayColor(rgb);
         }
+    }
+
+    /**
+     * Anti flicker mode
+     */
+    private void updateAntiFlicker() {
+        if (!mUseAntiFlicker) {
+            return;
+        }
+        mHardware.set(LineageHardwareManager.FEATURE_ANTI_FLICKER, isAntiFlickerEnabled());
     }
 
     /**
@@ -508,5 +538,18 @@ public class DisplayHardwareController extends LiveDisplayFeature {
             dst[1] = src[1];
             dst[2] = src[2];
         }
+    }
+
+    boolean isAntiFlickerEnabled() {
+        return mUseAntiFlicker &&
+                getBoolean(Settings.System.DISPLAY_ANTI_FLICKER, mDefaultAntiFlicker);
+    }
+
+    boolean setAntiFlickerEnabled(boolean enabled) {
+        if (!mUseAntiFlicker) {
+            return false;
+        }
+        putBoolean(Settings.System.DISPLAY_ANTI_FLICKER, enabled);
+        return true;
     }
 }