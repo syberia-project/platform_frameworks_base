24bb7e1c863b90b638dbb4670ade70e9c072ff03
TheHardGamer <gamelovr695@gmail.com>
2023-04-05 12:09:25 +0530

    DisplayPowerController: Disable log spam
    DisplayPowerController[0]: BrightnessEvent: disp=0, physDisp=local:4630946480857061761, brt=0.0, initBrt=0.0066815144, rcmdBrt=0.0066815144, preBrt=NaN, lux=0.0, preLux=0.0, hbmMax=1.0, hbmMode=off, rbcStrength=50, powerFactor=1.0, thrmMax=1.0, wasShortTermModelActive=false, flags=, reason=automatic [ dim ], autoBrightness=true
    
    Change-Id: Id2d3e875f56a9f453d757daa08476cb8b153f032
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -1823,7 +1823,6 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
             // they are transient.
             newEvent.adjustmentFlags = brightnessAdjustmentFlags;
             newEvent.flags |= (userSetBrightnessChanged ? BrightnessEvent.FLAG_USER_SET : 0);
-            Slog.i(TAG, newEvent.toString(/* includeTime= */ false));
 
             if (userSetBrightnessChanged) {
                 logManualBrightnessEvent(newEvent);
