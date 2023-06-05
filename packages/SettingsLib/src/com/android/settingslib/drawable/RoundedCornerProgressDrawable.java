4037b0fc92cfeb78da9bba0122cc2e5300c4d603
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-06-05 18:59:41 +0530

    base: Move redesigned seekbar to SettingsLib
    CTS does not allow modification to core material styles.
    
    Change-Id: Iee1303031c6a1efd44a07a93c1c31d318cda4f59
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SettingsLib/src/com/android/settingslib/drawable/RoundedCornerProgressDrawable.java
+++ packages/SettingsLib/src/com/android/settingslib/drawable/RoundedCornerProgressDrawable.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.internal.graphics.drawable;
+package com.android.settingslib.drawable;
 
 import android.content.pm.ActivityInfo;
 import android.content.res.Resources;
