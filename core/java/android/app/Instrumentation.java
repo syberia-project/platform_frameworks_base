28358ad5dd11fa8143c157f5d5b451c3944ae54e
Veena Arvind <aveena@google.com>
2023-06-06 18:42:42 +0000

    Increase timeout for UIAutomation
    In android U, the timeout was increased. Backporting this change to this branch to allow tests to pass for partner devices.
    
    Bug: 281653513
    
    Test: Partner device (b/281653513#comment29)
    Google: 2617830
    Change-Id: I0859bdd7005638e16b6d265f33132d17b47b7d0a
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -94,7 +94,7 @@ public class Instrumentation {
 
     private static final String TAG = "Instrumentation";
 
-    private static final long CONNECT_TIMEOUT_MILLIS = 5000;
+    private static final long CONNECT_TIMEOUT_MILLIS = 60000;
 
     /**
      * @hide
