66ac1fa10c811ce365a8b679dac91307b10d2425
Tommy Nordgren <tommy.nordgren@sony.com>
2023-09-12 13:37:36 +0200

    Global scale must always be updated when size compat scale is changed
    When an orientation change request that comes from the app differs
    from the orientation for the current activity the UI layout is not
    updated. This may lead to an un-intended layout size.
    
    Solve this by updating Global scale when size compat scale is changed.
    
    Test: run test apk
    Bug: 286384732
    Google: 2750502
    Change-Id: I1b3d93337c7700d58be94f5731abff932ea1895b

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -8322,7 +8322,11 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
 
     private void clearSizeCompatModeAttributes() {
         mInSizeCompatModeForBounds = false;
+        final float lastSizeCompatScale = mSizeCompatScale;
         mSizeCompatScale = 1f;
+        if (mSizeCompatScale != lastSizeCompatScale) {
+            forAllWindows(WindowState::updateGlobalScale, false /* traverseTopToBottom */);
+        }
         mSizeCompatBounds = null;
         mCompatDisplayInsets = null;
         mLetterboxUiController.clearInheritedCompatDisplayInsets();
@@ -8330,11 +8334,7 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
 
     @VisibleForTesting
     void clearSizeCompatMode() {
-        final float lastSizeCompatScale = mSizeCompatScale;
         clearSizeCompatModeAttributes();
-        if (mSizeCompatScale != lastSizeCompatScale) {
-            forAllWindows(WindowState::updateGlobalScale, false /* traverseTopToBottom */);
-        }
         // Clear config override in #updateCompatDisplayInsets().
         final int activityType = getActivityType();
         final Configuration overrideConfig = getRequestedOverrideConfiguration();
