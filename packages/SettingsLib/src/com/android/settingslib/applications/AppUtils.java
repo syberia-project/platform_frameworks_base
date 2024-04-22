d0dc86994b791bbee38a93ecbedcf59455928f2c
Yuan Xiangjing <yuan.xiangjing@zte.com.cn>
2024-04-22 11:12:41 +0800

    Fix synchronous lock failures
    Add a synchronized lock to prevent multiple competing threads
    from writing at the same time.
    
    Change-Id: If864ec07a35242eda6ee700ea814343e0008cedf
    Signed-off-by: Jia Jia <jia.jia@zte.com.cn>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SettingsLib/src/com/android/settingslib/applications/AppUtils.java
+++ packages/SettingsLib/src/com/android/settingslib/applications/AppUtils.java
@@ -307,8 +307,8 @@ public class AppUtils {
     }
 
     private static void setAppEntryMounted(ApplicationsState.AppEntry appEntry, boolean mounted) {
-        if (appEntry.mounted != mounted) {
-            synchronized (appEntry) {
+        synchronized (appEntry) {
+            if (appEntry.mounted != mounted) {
                 appEntry.mounted = mounted;
             }
         }
