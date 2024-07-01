nitrogen-project/android_frameworks_base

SystemUI: Readapt the Volte HD and voWiFi icons
Co-authored-by: xyyx <xyyx@mail.ru>

packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/binder/MobileIconBinder.kt
@@ -62,6 +62,7 @@ object MobileIconBinder {
         val roamingView = view.requireViewById<ImageView>(R.id.mobile_roaming)
         val roamingSpace = view.requireViewById<Space>(R.id.mobile_roaming_space)
         val dotView = view.requireViewById<StatusBarIconView>(R.id.status_bar_dot)
+        val volteView = view.requireViewById<ImageView>(R.id.mobile_volte)
 
         view.isVisible = true
         iconView.isVisible = true
@@ -156,6 +157,7 @@ object MobileIconBinder {
                         iconView.imageTintList = tintList
                         networkTypeView.imageTintList = tintList
                         roamingView.imageTintList = tintList
+                        volteView.imageTintList = tintList
                         activityIn.imageTintList = tintList
                         activityOut.imageTintList = tintList
                         dotView.setDecorColor(tint)
@@ -163,6 +165,17 @@ object MobileIconBinder {
                 }
 
                 launch { decorTint.collect { tint -> dotView.setDecorColor(tint) } }
+
+                launch {
+                    viewModel.volteId.distinctUntilChanged().collect { volteId ->
+                        if (volteId != 0) {
+                            volteView.visibility = VISIBLE
+                            volteView.setImageResource(volteId)
+                        } else {
+                            volteView.visibility = GONE
+                        }
+                    }
+                }
 
                 try {
                     awaitCancellation()

syberia-project/platform_frameworks_base

Change-Id: I8e19c47de1b2f05313b1f1ecdaab4a3f45e7719b
Signed-off-by: xyyx <xyyx@mail.ru>

packages/SystemUI/src/com/android/systemui/statusbar/pipeline/mobile/ui/binder/MobileIconBinder.kt
@@ -75,6 +75,7 @@ object MobileIconBinder {
         val roamingView = view.requireViewById<ImageView>(R.id.mobile_roaming)
         val roamingSpace = view.requireViewById<Space>(R.id.mobile_roaming_space)
         val dotView = view.requireViewById<StatusBarIconView>(R.id.status_bar_dot)
+        val volteView = view.requireViewById<ImageView>(R.id.mobile_volte)
 
         view.isVisible = viewModel.isVisible.value
         iconView.isVisible = true
@@ -246,6 +247,7 @@ object MobileIconBinder {
                             }
 
                             roamingView.imageTintList = tint
+                            volteView.imageTintList = tint
                             activityIn.imageTintList = tint
                             activityOut.imageTintList = tint
                             dotView.setDecorColor(colors.tint)
@@ -253,6 +255,17 @@ object MobileIconBinder {
                     }
 
                     launch { decorTint.collect { tint -> dotView.setDecorColor(tint) } }
+
+                    launch {
+                        viewModel.volteId.distinctUntilChanged().collect { volteId ->
+                            if (volteId != 0) {
+                                volteView.visibility = VISIBLE
+                                volteView.setImageResource(volteId)
+                            } else {
+                                volteView.visibility = GONE
+                            }
+                        }
+                    }
 
                     try {
                         awaitCancellation()